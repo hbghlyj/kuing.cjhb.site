fcd2fea2872574a4d18e36e926e7f05a7001365f
湖中沉 <341936031@qq.com>
2021-02-25 15:19:46 +0800

    Merge remote-tracking branch 'remotes/origin/master' into v3.5

--- upload/source/admincp/admincp_setting.php
+++ upload/source/admincp/admincp_setting.php
@@ -279,6 +279,12 @@ if(!submitcheck('settingsubmit')) {
 			array(2, $lang['setting_home_privacy_self']),
 			array(3, $lang['setting_home_privacy_register'])
 		)), $setting['privacy']['view']['index'], 'select');
+		showsetting('setting_home_privacy_view_profile', array('settingnew[privacy][view][profile]', array(
+			array(0, $lang['setting_home_privacy_alluser']),
+			array(1, $lang['setting_home_privacy_friend']),
+			array(2, $lang['setting_home_privacy_self']),
+			array(3, $lang['setting_home_privacy_register'])
+		)), $setting['privacy']['view']['profile'], 'select');
 		showsetting('setting_home_privacy_view_friend', array('settingnew[privacy][view][friend]', array(
 			array(0, $lang['setting_home_privacy_alluser']),
 			array(1, $lang['setting_home_privacy_friend']),
@@ -2481,6 +2487,7 @@ EOT;
 		exit;
 
 	} elseif ($operation == 'antitheft'){
+		/*search={"setting_antitheft":"action=setting&operation=antitheft"}*/
 		if($_GET['anchor'] == 'iplist') {
 
 			if(submitcheck('antitheftsubmit', true)) {
@@ -2564,6 +2571,7 @@ EOT;
 			showtablefooter();
 			showsubmit('settingsubmit');
 		}
+		/*search*/
 		exit;
 
 	} else {
