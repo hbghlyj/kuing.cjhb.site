c87065ec20147526d3f18a01c95de7c1bd6a72b2
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-03-24 13:46:12 +0000

    slug、边栏显示用户名、去除页脚版权

--- src/views/partial/head.php
+++ src/views/partial/head.php
@@ -5,7 +5,7 @@
     <base href="<?= BASE_URL ?>">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php include 'kk/mathjax.php';?>
 
     <title><?= ucfirst(str_replace('-',' ',$PageTitle)) ?></title>
 
