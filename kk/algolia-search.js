e85cd8e0cb479b4931a832fc82e8617d1b313447
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-03-31 16:28:03 +0100

    搜索界面改进： 1.还应显示作者和总楼层数 2.关键字在内容中突出显示，但也应在标题和描述中突出显示

--- kk/algolia-search.js
+++ kk/algolia-search.js
@@ -49,7 +49,7 @@ window.addEventListener('load', function () {
 <div class="ais-hits--content">
 	<h2 itemprop="name headline"><a href="/forum.php?mod=redirect&goto=findpost&pid=${data.objectID}" class="ais-hits--title-link" itemprop="url">${data._highlightResult.title.value}</a> <span style="color: #666;font-weight:normal;">${data.author} (${data.totalposts - 1} ${lang['replies']}) ${data.date}</span></h2>
 	<div class="excerpt">
-		<p><span class="suggestion-post-content ais-hits--content-snippet">${data._snippetResult['content'].value}</span></p>
+		<p><span class="suggestion-post-content ais-hits--content-snippet">${data._highlightResult.description.value}${data._snippetResult['content'].value}</span></p>
 	</div>
 </div>
 </article>`;
