2b409e7ab5d892a2224d35a17d94e5a90e9a3a52
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-04-06 11:55:13 +0100

    通过把innerHTML改为innerText解决了再次点击👁变回公式，此时右键公式 -> show math as -> tex commands，则 & 变成了 &amp;

--- kk/zdy3pc.js
+++ kk/zdy3pc.js
@@ -76,27 +76,28 @@ document.querySelectorAll('.t_f').forEach(post => {
     });
 });
 
+const show_math_code = function(){
+    MathJax.startup.document.getMathItemsWithin(this.parentElement.nextElementSibling).forEach(function (item) {
+        [item.math,item.typesetRoot.innerHTML]=[item.typesetRoot[item.typesetRoot.firstElementChild ? "innerHTML" : "innerText"],item.math];
+        if(item.start.n){
+            item.math = item.math.slice(item.start.n);
+            item.start.n = 0;
+        }else{
+            item.typesetRoot.prepend(item.start.delim);
+            item.start.n = item.start.delim.length;
+        }
+        if(item.end.n){
+            item.math = item.math.slice(0,-item.end.n);
+            item.end.n = 0;
+        }else{
+            item.typesetRoot.append(item.end.delim);
+            item.end.n = item.end.delim.length;
+        }
+    });
+};
 document.querySelectorAll('.plc .pi').forEach(plcpi => {
     const eye = document.createElement("a");
-    eye.addEventListener("click", function(){
-        MathJax.startup.document.getMathItemsWithin(this.parentElement.nextElementSibling).forEach(function (item) {
-            [item.math,item.typesetRoot[item.typesetRoot.firstElementChild ? "innerText" : "innerHTML"]]=[item.typesetRoot[item.typesetRoot.firstElementChild ? "innerHTML" : "innerText"],item.math];
-            if(item.start.n){
-                item.math = item.math.slice(item.start.n);
-                item.start.n = 0;
-            }else{
-                item.typesetRoot.prepend(item.start.delim);
-                item.start.n = item.start.delim.length;
-            }
-            if(item.end.n){
-                item.math = item.math.slice(0,-item.end.n);
-                item.end.n = 0;
-            }else{
-                item.typesetRoot.append(item.end.delim);
-                item.end.n = item.end.delim.length;
-            }
-        });
-    });
+    eye.addEventListener("click", show_math_code);
     eye.style = "float:right;margin-left:5px;cursor:pointer;";
     eye.innerHTML = "&#x1f441;";
     eye.title = "显示公式代码";
