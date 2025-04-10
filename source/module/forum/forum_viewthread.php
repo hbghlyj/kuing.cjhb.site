9f6a2046c4c67de987a052ccd6c46db7385c32d4
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-04-10 14:07:39 +0100

    我还以为本人编辑才有这 5 分钟没痕迹的设定，原来他人修改也如此？我觉得他人编辑不应该有这设定

--- source/module/forum/forum_viewthread.php
+++ source/module/forum/forum_viewthread.php
@@ -1310,7 +1310,7 @@ function viewthread_procpost($post, $lastvisit, $ordertype, $maxposition = 0) {
 	$_G['forum_firstpid'] = intval($_G['forum_firstpid']);
 	$post['numbercard'] = viewthread_numbercard($post);
 	$post['mobiletype'] = getstatus($post['status'], 4) ? base_convert(getstatus($post['status'], 10).getstatus($post['status'], 9).getstatus($post['status'], 8), 2, 10) : 0;
-	if($_G['setting']['editedby'] && $post['lastupdate'] && $post['lastupdate'] - $post['dbdateline'] > 300) {
+	if($_G['setting']['editedby'] && ($post['lastupdate'] && $post['lastupdate'] - $post['dbdateline'] > 300 || $post['updateuid'] && $post['updateuid'] != $post['authorid'])) {
 		if (DISCUZ_LANG == 'EN/') {
 			$post['message'] = ' at '.dgmdate($post['lastupdate'], 'u', '9999', getglobal('setting/dateformat').' H:i:s').'</i>'.$post['message'];
 		} else {
