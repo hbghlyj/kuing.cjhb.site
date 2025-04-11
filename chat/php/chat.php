ce375f6a24b5550868c4dedc860afddefbe95ea5
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-04-11 21:49:29 +0800

    PRIMARY KEY (`time`)

--- chat/php/chat.php
+++ chat/php/chat.php
@@ -35,7 +35,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// CREATE TABLE chat (time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, uid mediumint NOT NULL, author CHAR(15) NOT NULL, message TEXT NOT NULL );
+// CREATE TABLE chat (time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, uid mediumint NOT NULL, author CHAR(15) NOT NULL, message TEXT NOT NULL, PRIMARY KEY (`time`) );
 $stmt = $conn->prepare("INSERT INTO chat (uid,author,message) VALUES (?, ?, ?)");
 $stmt->bind_param("iss", $_G['uid'], $_G['username'], $options['text']);
 $stmt->execute();
