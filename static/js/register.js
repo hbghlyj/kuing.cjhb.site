090c1114b1f44d07a8270aaa4ce9d0a6474f5b84
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-12 20:11:11 +0100

    将checkemail改为了只用Javascript完成

--- static/js/register.js
+++ static/js/register.js
@@ -6,7 +6,7 @@
 	Modified by Valery Votintsev, codersclub.org
 */
 
-/*vot*/ var lastusername = '', lastpassword = '', lastemail = '', lastinvitecode = '', stmp = new Array(), modifypwd = false, profileTips = lng['leave_blank_old_pass'];
+/*vot*/ var lastusername = '', lastpassword = '', lastinvitecode = '', stmp = new Array(), modifypwd = false, profileTips = lng['leave_blank_old_pass'];
 
 function errormessage(id, msg) {
 	if($(id)) {
@@ -339,20 +339,7 @@ function checkpassword(id1, id2) {
 }
 
 function checkemail(id) {
-	errormessage(id);
-	var email = trim($(id).value);
-	if($(id).parentNode.className.match(/ p_right/) && (email == '' || email == lastemail)) {
-		return;
-	} else {
-		lastemail = email;
-	}
-	if(!email.match(
-		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-	  )) {
-/*vot*/		errormessage(id, lng['email_invalid']);
-		return;
-	}
-	$('tip_' + id).parentNode.className = $('tip_' + id).parentNode.className.replace(/ p_right/, '');
+/*vot*/	errormessage(id, trim($(id).value).match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? 'succeed' : lng['email_invalid']);
 }
 
 function checkinvite() {
