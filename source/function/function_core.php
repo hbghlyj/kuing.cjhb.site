26bc9d8f552ce5bbd217e9d35a90880cd020f535
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-12 14:43:07 +0100

    checkMobile

--- source/function/function_core.php
+++ source/function/function_core.php
@@ -342,42 +342,11 @@ function checkrobot() {
 	}
 }
 function checkmobile() {
-	global $_G;
-	static $touchbrowser_list =array('iphone', 'android', 'phone', 'mobile', 'wap', 'netfront', 'java', 'opera mobi', 'opera mini',
-				'ucweb', 'windows ce', 'symbian', 'series', 'webos', 'sony', 'blackberry', 'dopod', 'nokia', 'samsung',
-				'palmsource', 'xda', 'pieplus', 'meizu', 'midp', 'cldc', 'motorola', 'foma', 'docomo', 'up.browser',
-				'up.link', 'blazer', 'helio', 'hosin', 'huawei', 'novarra', 'coolpad', 'webos', 'techfaith', 'palmsource',
-				'alcatel', 'amoi', 'ktouch', 'nexian', 'ericsson', 'philips', 'sagem', 'wellcom', 'bunjalloo', 'maui', 'smartphone',
-				'iemobile', 'spice', 'bird', 'zte-', 'longcos', 'pantech', 'gionee', 'portalmmm', 'jig browser', 'hiptop',
-				'benq', 'haier', '^lct', '320x320', '240x320', '176x220', 'windows phone');
-	static $wmlbrowser_list = array('cect', 'compal', 'ctl', 'lg', 'nec', 'tcl', 'alcatel', 'ericsson', 'bird', 'daxian', 'dbtel', 'eastcom',
-			'pantech', 'dopod', 'philips', 'haier', 'konka', 'kejian', 'lenovo', 'benq', 'mot', 'soutec', 'nokia', 'sagem', 'sgh',
-			'sed', 'capitel', 'panasonic', 'sonyericsson', 'sharp', 'amoi', 'panda', 'zte');
-
-	static $pad_list = array('ipad');
-
-	$useragent = $_SERVER['HTTP_USER_AGENT'];
-
-	if(dstrpos($useragent, $pad_list)) {
-		return false;
-	}
-	if(($v = dstrpos($useragent, $touchbrowser_list, true))){
-		$_G['mobile'] = $v;
-		return '2';
-	}
-	if(($v = dstrpos($useragent, $wmlbrowser_list))) {
-		$_G['mobile'] = $v;
-		return '3'; //wml版
-	}
-	$brower = array('mozilla', 'chrome', 'safari', 'opera', 'm3gate', 'winwap', 'openwave');
-	if(dstrpos($useragent, $brower)) return false;
-
-	$_G['mobile'] = '';
-	if(isset($_G['mobiletpl'][$_GET['mobile']])) {
-		return true;
-	} else {
-		return false;
-	}
+    global $_G;
+	if(empty($_SERVER['HTTP_USER_AGENT'])) {
+        return $_G['mobile'] = false;
+    }
+    return $_G['mobile'] = preg_match("/android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos/i", $_SERVER['HTTP_USER_AGENT']) ? true : false;
 }
 
 function dstrpos($string, $arr, $returnvalue = false) {
