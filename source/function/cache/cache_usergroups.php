708254e44405be9f676037042c823f22f6580174
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-13 15:57:01 +0100

    Close #117

--- source/function/cache/cache_usergroups.php
+++ source/function/cache/cache_usergroups.php
@@ -17,8 +17,8 @@ function build_cache_usergroups() {
 
 	$data = [];
 	foreach(table_common_usergroup::t()->range_orderby_creditshigher() as $key => $value) {
-		$group = array_merge(['groupid' => $value['groupid'], 'upgroupid' => $value['upgroupid'], 'type' => $value['type'], 'grouptitle' => $value['grouptitle'], 'creditshigher' => $value['creditshigher'], 'creditslower' => $value['creditslower'], 'stars' => $value['stars'], 'color' => $value['color'], 'icon' => $value['icon'], 'system' => $value['system']], $data_uf[$key]);
-		if($group['type'] == 'special') {
+		$group = array_merge(['groupid' => $value['groupid'], 'upgroupid' => $value['upgroupid'], 'type' => $value['type'], 'grouptitle' => $value['grouptitle'], 'grouptitle_en' => $value['grouptitle_en'], 'creditshigher' => $value['creditshigher'], 'creditslower' => $value['creditslower'], 'stars' => $value['stars'], 'color' => $value['color'], 'icon' => $value['icon'], 'system' => $value['system']], $data_uf[$key]);
+	    if($group['type'] == 'special') {
 			if($group['system'] != 'private') {
 				list($dailyprice) = explode("\t", $group['system']);
 				$group['pubtype'] = $dailyprice > 0 ? 'buy' : 'free';
