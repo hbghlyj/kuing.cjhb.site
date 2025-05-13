acd7a1a88ca38ee700dd857a17f5827f19c546b7
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-13 02:44:51 +0100

    remove unused 'connectappkey'

--- source/plugin/googleconnect/connect.class.php
+++ source/plugin/googleconnect/connect.class.php
@@ -2,23 +2,26 @@
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
-
 class plugin_googleconnect {
-	
-	function __construct() {
+	public function global_login_extra() {
 		global $_G;
-		$_G['setting']['pluginhooks']['register_logging_method'] = '<script src="https://accounts.google.com/gsi/client" async></script>
+		$_G['setting']['pluginhooks']['logging_method'] = $_G['setting']['pluginhooks']['register_logging_method'] = '<div class="g_id_signin"
+data-type="standard"
+data-shape="pill"
+data-theme="filled_blue"
+data-text="continue_with"
+data-size="large"
+data-logo_alignment="left"
+     data-width="40">
+</div>';
+	$_G['setting']['pluginhooks']['logging_method'] .= '<script src="https://accounts.google.com/gsi/client" async></script>';
+		return '<script src="https://accounts.google.com/gsi/client" async></script>
 		<div id="g_id_onload"
 			data-client_id="'.$_G['setting']['connectappid'].'"
-			data-login_uri="connect.php?mod=login&op=callback">
-		</div>
-		<div class="g_id_signin"
-			data-type="standard"
-			data-size="large"
-			data-theme="outline"
-			data-text="sign_in_with"
-			data-shape="rectangular"
-			data-logo_alignment="left">
+			data-context="signin"
+			data-login_uri="connect.php?mod=login&op=callback"
+			data-auto_select="true"
+			data-itp_support="true">
 		</div>';
 	}
 }
