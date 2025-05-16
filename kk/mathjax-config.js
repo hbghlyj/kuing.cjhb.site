30198adb983413195d9c7dda8190494aea6d00d4
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-16 13:18:48 +0100

    使每次MathJax.typeset时运行[去行间公式后的1个<br>]，而不是只在页面加载时

---
+++ kk/mathjax-config.js
@@ -0,0 +1,99 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [ ['$','$'], ['`','`'], ["\\(","\\)"] ],
+    processEscapes: true,
+    tags: "ams",
+    macros: {
+      mbb: '\\mathbb',
+      riff: '\\implies',
+      liff: '\\impliedby',
+      abs: ['\\left\\lvert #1\\right\\rvert', 1],
+      rmd: '\\mathop{}\\!\\mathrm{d}',
+      vv: '\\overrightarrow',
+      sslash: '\\mathrel{/\\mkern-5mu/}',
+      px: '\\mathrel{/\\mkern-5mu/}',
+      pqd: '\\stackrel{\\smash[b]{/\\!/}}{\\raise-.3ex{=}}',
+      veps: '\\varepsilon',
+      du: '^\\circ',
+      bsb: '\\boldsymbol',
+      bm: '\\boldsymbol',
+      kongji: '\\varnothing',
+      buji: '\\complement',
+      S: ['S_{\\triangle #1}', 1],
+      led: '\\left\\{\\begin{aligned}',
+      endled: '\\end{aligned}\\right.',
+      edr: '\\left.\\begin{aligned}',
+      endedr: '\\end{aligned}\\right\\}',
+      an: '\\{a_n\\}',
+      bn: '\\{b_n\\}',
+      cn: '\\{c_n\\}',
+      xn: '\\{x_n\\}',
+      Sn: '\\{S_n\\}',
+      inR: '\\in\\mbb R',
+      inN: '\\in\\mbb N',
+      inZ: '\\in\\mbb Z',
+      inC: '\\in\\mbb C',
+      inQ: '\\in\\mbb Q',
+      Rtt: '\\text{Rt}\\triangle',
+      LHS: '\\text{LHS}',
+      RHS: '\\text{RHS}',
+      arccot: '\\operatorname{arccot}',
+      arcsinh: '\\operatorname{arcsinh}',
+      arccosh: '\\operatorname{arccosh}',
+      arctanh: '\\operatorname{arctanh}',
+      arccoth: '\\operatorname{arccoth}',
+    },
+    autoload: {
+      color: [],
+      colorv2: ['color']
+    },
+    packages: {'[+]': ['noerrors','mathtools','xypic']}
+  },
+  options: {
+    ignoreHtmlClass: 'blockcode',
+    menuOptions: {
+      settings: {
+        zoom: "DoubleClick"
+      }
+    },
+    processHtmlClass: 'tex2jax_process',
+    renderActions: {
+      assistiveMml: [],
+      addTeX: [151,
+        (doc) => {
+            for (const math of doc.math) {
+              if (math.state() >= 200) return;
+              const spanElement = document.createElement("span");
+              spanElement.style.position = "absolute";
+              spanElement.style.left = "-9999px";
+              spanElement.style.opacity = "0";
+              spanElement.textContent = math.start.delim + math.math + math.end.delim;
+              math.typesetRoot.appendChild(spanElement);
+              if(math.math.length < 100) { doc.adaptor.setAttribute(math.typesetRoot, 'title', math.math); }
+            }
+        },
+        ''
+      ],
+      //去行间公式后的1个br
+      removeBr: [202,
+        (doc) => {
+          for (const math of doc.math) {
+            if (math.display && math.typesetRoot.nextSibling?.nodeType === Node.ELEMENT_NODE && math.typesetRoot.nextSibling.matches('br')) {
+              math.typesetRoot.nextSibling.remove();
+            }
+          }
+        },
+        ''
+      ],
+    }
+  },
+  loader: {
+    load: ['[tex]/noerrors','[tex]/mathtools','[custom]/xypic.js'],
+    //paths: {custom: '//cdn.jsdelivr.net/gh/sonoisa/XyJax-v3@3.0.1/build'}
+    paths: {custom: 'kk'}
+  },
+  svg: {
+    scale: 0.9,
+    fontCache: 'global'
+  }
+};
