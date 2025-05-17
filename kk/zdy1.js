98159ac5c89f28168fc8a850ff524471ec2bf49a
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-17 15:11:00 +0100

    板块描述居左以节省宽度

--- kk/zdy1.js
+++ kk/zdy1.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Find all forum links within the table
     const forumLinks = document.querySelectorAll(
-        'table.fl_tb h2 a[href^="forum.php?mod=forumdisplay&fid="],' +
-        'table.fl_tb td.fl_by a[href^="forum.php?mod=redirect&tid="]'
+        'table.fl_tb :is(.fl_by,.fl_icn) a[href^="forum.php?mod='
     );
 
     forumLinks.forEach(link => {
@@ -20,10 +19,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // hover background
         parentTd.addEventListener('mouseenter', () => {
-            parentTd.style.backgroundColor = '#ffe';
+            parentTd.style.backgroundColor = '#f9f9f9';
+            parentTd.style.boxShadow = 'inset 0px 0px 5px #c1c1c1';
         });
         parentTd.addEventListener('mouseleave', () => {
             parentTd.style.backgroundColor = '';
+            parentTd.style.boxShadow = '';
         });
     });
 });
