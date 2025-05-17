e993f4b7758c87e1068a71dc8ec639a3e2ba3a34
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-17 13:52:07 +0100

    By default expand chatbox on index page and collapse chat on forum.php

--- chat/PusherChatWidget.js
+++ chat/PusherChatWidget.js
@@ -75,11 +75,12 @@ function PusherChatWidget(pusher, options) {
       }
     });
   }
-  // Read collapse/expand status from cookie
+ // Read collapse/expand status from cookie
  if(document.cookie.replace(/(?:(?:^|.*;\s*)isCollapsed\s*\=\s*([^;]*).*$)|^.*$/, "$1") == ''){
     document.cookie = "isCollapsed=true; path=/forum.php";
     document.cookie = "isCollapsed=true; path=/member.php";
     document.cookie = "isCollapsed=true; path=/connect.php";
+    document.cookie = "isCollapsed=true; path=/search.php";
     document.cookie = "isCollapsed=true; path=/misc.php";
     document.cookie = "isCollapsed=true; path=/home.php";
   }
