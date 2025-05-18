09caaf119a50213afaff07db2d8c6cae241c4fd6
湖中沉 <341936031@qq.com>
2025-05-18 03:31:40 +0000

    !2455 【X5】修复计划任务中的cron_cleanup_daily.php每日数据清理未使用附件的清理问题 * 修复计划任务中的cron_cleanup_daily.php每日数据清理未使用附件的清理问题

--- source/class/table/table_forum_attachment_unused.php
+++ source/class/table/table_forum_attachment_unused.php
@@ -30,7 +30,7 @@ class table_forum_attachment_unused extends discuz_table {
 	public function clear() {
 		require_once libfile('function/forum');
 		$delaids = [];
-		$query = DB::query('SELECT aid, attachment, thumb FROM %t WHERE %i', [$this->_table, DB::field('dateline', TIMESTAMP - 86400)]);
+		$query = DB::query('SELECT aid, attachment, thumb FROM %t WHERE %i', [$this->_table, DB::field('dateline', TIMESTAMP - 86400, '<')]);
 		while($attach = DB::fetch($query)) {
 			updatemembercount($attach['uid'], array('todayattachs' => -1, 'todayattachsize' => -$attach['filesize'], 'attachsize' => -$attach['filesize']), false);
 			dunlink($attach);
@@ -38,7 +38,7 @@ class table_forum_attachment_unused extends discuz_table {
 		}
 		if($delaids) {
 			DB::query('DELETE FROM %t WHERE %i', ['forum_attachment', DB::field('aid', $delaids)], false, true);
-			DB::query('DELETE FROM %t WHERE %i', [$this->_table, DB::field('dateline', TIMESTAMP - 86400)], false, true);
+			DB::query('DELETE FROM %t WHERE %i', [$this->_table, DB::field('dateline', TIMESTAMP - 86400, '<')], false, true);
 		}
 	}
 
