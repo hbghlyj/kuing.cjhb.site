f7e3963503d914c483c6b95d8a2ac9066dcf6ec0
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-21 01:59:24 +0100

    下载的模板会在 template 文件夹里 以后有哪些困难的，可以参考它们

---
+++ template/ahome_common/cache.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *      This is NOT a freeware, use is subject to license terms
+ *      应用名称: 小时空通用套系 免费版 1.9
+ *      下载地址: https://addon.dismall.com/templates/ahome_common.html
+ *      应用开发者: 小时空
+ *      开发者QQ: 3033199665
+ *      更新日期: 202505210601
+ *      授权域名: kuing.cjhb.site
+ *      授权码: 2025052022pB747ZS4bS
+ *      未经应用程序开发者/所有者的书面许可，不得进行反向工程、反向汇编、反向编译等，不得擅自复制、修改、链接、转载、汇编、发表、出版、发展与之有关的衍生产品、作品等
+ */
+
+//FileCache
+if(!defined('IN_ADMINCP')) {
+	exit('Access Denied');
+}
