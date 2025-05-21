1afe11ed1fd45304f3c475c0a592c943aa0e8326
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-21 22:10:17 +0100

    你的时间感非常准确，我已经找到了时间显示不准确的原因（你说的时间加速），应该使用floor函数而不是round函数

--- chat/PusherChatWidget.js
+++ chat/PusherChatWidget.js
@@ -333,9 +333,9 @@ PusherChatWidget.timeToDescription = function(time) {
   const date = new Date(time);
   const howLongAgo = now - date;
   let desc;
-  const seconds = Math.round(howLongAgo/1000);
-  const minutes = Math.round(seconds/60);
-  const hours = Math.round(minutes/60);
+  const seconds = Math.floor(howLongAgo/1000);
+  const minutes = Math.floor(seconds/60);
+  const hours = Math.floor(minutes/60);
   if(seconds <= 0) {
     desc = isChinese ? "刚刚" : "just now";
   }
