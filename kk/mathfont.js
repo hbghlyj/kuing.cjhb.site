9128d5a328686dd4d4de5b676df555b61140a218
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-05-24 02:40:33 +0100

    directly styling the <span> inside the <option> is unlikely to work reliably:
    The browser cannot style nested elements within <option>
    
    It will inherit font-family of the <select> element, styling nested elements within <option> remains particularly challenging and inconsistent.

--- kk/mathfont.js
+++ kk/mathfont.js
@@ -91,8 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 		for (let font of localFonts) {
 			let option = document.createElement("option");
-			option.value = font;
-			option.innerHTML = '<span style="font-family: ' + font + '">' + font + '</span>';
+			option.value = option.innerText = font;
 			mathfont_select.appendChild(option);
 			if (font === fontFamilyCookie) {
 			  option.selected = true;
