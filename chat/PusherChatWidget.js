217f9df57d4650f61ff7c1fc8a91e1fa456a05e0
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-02 15:17:26 +0100

    live message scroll animation

--- chat/PusherChatWidget.js
+++ chat/PusherChatWidget.js
@@ -361,8 +361,9 @@ PusherChatWidget.prototype._actuallyAppendMessage = function(entry, oldScrollHei
   ++this._itemCount;
 
   if (entry.isLiveMessage) {
-    // Scroll to the bottom for new messages
-    this._messagesEl.animate({scrollTop: this._messagesEl[0].scrollHeight}, 500);
+    if (!this.isCollapsed && (this._messagesEl.scrollTop() + this._messagesEl.innerHeight() >= this._messagesEl[0].scrollHeight - entry.messageEl.outerHeight() * 1.5)) {
+      this._messagesEl.animate({scrollTop: this._messagesEl[0].scrollHeight}, 500);
+    }
   } else if (entry.isPrepending) {
     // Restore scroll position after prepending new content
     var newScrollHeight = this._messagesEl[0].scrollHeight;
