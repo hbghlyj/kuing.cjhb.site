f8153e5bf3c90dad52b2975967561e57caada84f
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-11 15:48:30 +0100

    Fix route loader and persist patch

---
+++ src/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+define('TITLE', 'DocPHT');
+define('DS', DIRECTORY_SEPARATOR);
+define('BASE_PATH', __DIR__ . DS);
+define('SUBTITLE', '');
+define('DOWNLOAD', '');
+define('GITHUB', '');
+define('LANGUAGE', 'en_EN');
+define('TIMEZONE', 'UTC');
+define('DATAFORMAT', 'Y-m-d');
+define('BASE_URL', 'http://localhost/doc.php/');
+define('DOMAIN_NAME', $_SERVER['HTTP_HOST']);
+define('SMTPMAILER', false);
+define('SMTPHOST', '');
+define('SMTPPORT', '');
+define('SMTPENCRYPT', '');
+define('SMTPUSERNAME', '');
+define('SMTPPASSWORD', '');
