c36c040635c8759e02e6869668bd5d10b4436924
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-13 13:05:57 +0100

    Optimize desktop excerpt generation

--- source/module/forum/forum_forumdisplay.php
+++ source/module/forum/forum_forumdisplay.php
@@ -992,8 +992,9 @@ if($_G['forum']['status'] == 3) {
 }
 
 $threadlist_data = array();
-if(defined('IN_MOBILE') && $_G['forum_threadcount']) {
-	$threadlist_data = get_attach($_G['forum_threadlist']);
+if($_G['forum_threadcount']) {
+        // Desktop thread list only needs excerpts, skip fetching attachments
+        $threadlist_data = get_attach($_G['forum_threadlist'], false, false, false);
 }
 if(!defined('IN_ARCHIVER')) {
 	include template($template);
