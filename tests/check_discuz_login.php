f651bccfab5c7bc7f5cc91a3ccf9d4df8e7b3d50
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-17 07:58:14 +0800

    Update tests/check_discuz_login.php

--- tests/check_discuz_login.php
+++ tests/check_discuz_login.php
@@ -1,18 +1,9 @@
 <?php
-// Quick script to verify Discuz login status
-define('DISCUZ_BRIDGE_DEBUG', true);
-require __DIR__ . '/../src/core/constants.php';
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/core/Helper/DiscuzBridge.php';
 DocPHT\Core\Helper\DiscuzBridge::syncSession();
-require __DIR__ . '/../config/config_global.php';
-require_once __DIR__ . '/../source/class/class_core.php';
-
-$discuz = C::app();
-$discuz->init();
-
-if (!empty($_G['uid'])) {
-    echo "Logged in as {$_G['username']} (uid={$_G['uid']})\n";
+if (isset($_SESSION['Active']) && $_SESSION['Username']) {
+    echo "Logged in as {$_SESSION['Username']}\n";
 } else {
     echo "Not logged in\n";
 }
