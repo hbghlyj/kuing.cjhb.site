25e7e2073f3347591bd3c5e4122424a3c0576d67
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-17 01:45:26 +0100

    Restore member count update in attachment cleanup

--- source/class/table/table_forum_attachment_unused.php
+++ source/class/table/table_forum_attachment_unused.php
@@ -24,16 +24,16 @@ class table_forum_attachment_unused extends discuz_table
 	public function clear() {
 		require_once libfile('function/forum');
 		$delaids = array();
-		$query = DB::query("SELECT aid, attachment, thumb FROM %t WHERE %i", array($this->_table, DB::field('dateline', TIMESTAMP - 86400)));
-		while($attach = DB::fetch($query)) {
-			updatemembercount($attach['uid'], array('todayattachs' => -1, 'todayattachsize' => -$attach['filesize'], 'attachsize' => -$attach['filesize']), false);
-			dunlink($attach);
-			$delaids[] = $attach['aid'];
-		}
-		if($delaids) {
-			DB::query("DELETE FROM %t WHERE %i", array('forum_attachment', DB::field('aid', $delaids)), false, true);
-			DB::query("DELETE FROM %t WHERE %i", array($this->_table, DB::field('dateline', TIMESTAMP - 86400)), false, true);
-		}
+               $query = DB::query("SELECT aid, attachment, thumb FROM %t WHERE %i", array($this->_table, DB::field('dateline', TIMESTAMP - 86400, '<')));
+               while($attach = DB::fetch($query)) {
+                       updatemembercount($attach['uid'], array('todayattachs' => -1, 'todayattachsize' => -$attach['filesize'], 'attachsize' => -$attach['filesize']), false);
+                       dunlink($attach);
+                       $delaids[] = $attach['aid'];
+               }
+               if($delaids) {
+                       DB::query("DELETE FROM %t WHERE %i", array('forum_attachment', DB::field('aid', $delaids)), false, true);
+                       DB::query("DELETE FROM %t WHERE %i", array($this->_table, DB::field('dateline', TIMESTAMP - 86400, '<')), false, true);
+               }
 	}
 
 }
