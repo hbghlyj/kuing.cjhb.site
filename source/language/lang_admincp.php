1782e617edff4188dbdb89b08c40e5cc6a9f4ed4
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-18 06:07:32 +0100

    Fix formhash check in tag suggestion handler

--- source/language/lang_admincp.php
+++ source/language/lang_admincp.php
@@ -2603,8 +2603,10 @@ $lang = array
 	'tagname' => '标签名称',
 	'tag_status' => '标签状态',
 	'tag_recommend_comment' => '每行一个推荐标签，标签长度在3-20个字符内',
-	'tag_admin' => '标签管理',
-	'tag_result' => '符合条件的标签数',
+        'tag_admin' => '标签管理',
+       'pending_tag_suggest' => '待审核标签建议',
+       'error_invalid_id' => '无效的ID',
+        'tag_result' => '符合条件的标签数',
 
 	'collection' => '淘帖管理',
 	'collection_date' => '发表时间',
