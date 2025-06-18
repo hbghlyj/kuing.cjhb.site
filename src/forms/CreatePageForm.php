0668c75e220fae7fe3ad20250ba21f12d17008f4
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-18 21:02:14 +0100

    feat: suggest page creation on missing page

--- src/forms/CreatePageForm.php
+++ src/forms/CreatePageForm.php
@@ -13,73 +13,82 @@
 
 namespace DocPHT\Form;
 
+use DocPHT\Core\Translator\T;
+use DocPHT\Model\PageModel;
 use Nette\Forms\Form;
 use Nette\Utils\Html;
-use DocPHT\Core\Translator\T;
 
 class CreatePageForm extends MakeupForm
 {
-    
+    /**
+     * @var PageModel
+     */
+    protected $pageModel;
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->pageModel = new PageModel();
+    }
+
+    /**
+     * @return Form
+     */
     public function create()
     {
-        
-        $languages = $this->doc->listCodeLanguages();
-        $options = $this->doc->getOptions();
-
         $form = new Form;
         $form->onRender[] = [$this, 'bootstrap4'];
 
         $form->addGroup(T::trans('Create new page'));
 
-        $getTopic = $this->pageModel->getUniqTopics();
-
         $form->addText('topic', T::trans('Topic'))
-            ->setDefaultValue(isset($_GET['topic']) ? $_GET['topic'] : '')
-        	->setHtmlAttribute('placeholder', T::trans('Enter topic'))
-            ->setAttribute('list', 'topicList')
-            ->setAttribute('autocomplete', 'off')
-            ->setRequired(T::trans('Enter topic'));
-        	
-        $dataList = Html::el('datalist id="topicList"');
-        
-        if (is_array($getTopic)) {
-            foreach ($getTopic as $value) {
-                $dataList->create('option value="'.str_replace('-',' ',$value).'"');
-            }
-            echo $dataList;
-        }
-        
-        
-        $form->addText('mainfilename', T::trans('Page name'))
-        ->setDefaultValue(isset($_GET['mainfilename']) ? $_GET['mainfilename'] : '')
-        ->setHtmlAttribute('placeholder', T::trans('Enter page name'))
-        ->setAttribute('autocomplete', 'off')
-        ->setRequired(T::trans('Enter page name'));
-        
-        $form->addProtection(T::trans('Security token has expired, please submit the form again'));
-        
+            ->setHtmlAttribute('placeholder', T::trans('Enter topic'))
+            ->setRequired(T::trans('Topic is required.'))
+            ->setAttribute('onkeyup', 'this.value = this.value.toLowerCase();')
+            ->setAttribute('pattern','[a-z0-9]+(?:-[a-z0-9]+)*')
+            ->addRule(Form::PATTERN, T::trans('Must be alphanumeric and lowercase, use a hyphen for spaces.'), '[a-z0-9]+(?:-[a-z0-9]+)*')
+            ->setDefaultValue(isset($_GET['topic']) ? htmlspecialchars($_GET['topic'], ENT_QUOTES, 'UTF-8') : '');
+
+        $form->addText('filename', T::trans('Filename'))
+            ->setHtmlAttribute('placeholder', T::trans('Enter filename'))
+            ->setRequired(T::trans('Filename is required.'))
+            ->setAttribute('onkeyup', 'this.value = this.value.toLowerCase();')
+            ->setAttribute('pattern','[a-z0-9]+(?:-[a-z0-9]+)*')
+            ->addRule(Form::PATTERN, T::trans('Must be alphanumeric and lowercase, use a hyphen for spaces.'), '[a-z0-9]+(?:-[a-z0-9]+)*')
+            ->setDefaultValue(isset($_GET['filename']) ? htmlspecialchars($_GET['filename'], ENT_QUOTES, 'UTF-8') : '');
+
+        $form->addTextArea('description', T::trans('Description'))
+            ->setHtmlAttribute('placeholder', T::trans('Enter a description'))
+            ->setRequired(T::trans('Description is required.'));
+
+        $form->addUpload('file', T::trans('Add an image or a code file'))
+            ->setRequired(false)
+            ->addRule(Form::MIME_TYPE, 'Not a valid file.', ['image/gif,image/png,image/jpeg,image/svg+xml,application/zip,application/x-rar-compressed,application/octet-stream,text/plain,text/x-c,text/x-c++,text/x-c-header,text/x-c-source,text/x-d,text/x-pascal,text/x-fortran,text/x-asm,text/x-java-source,text/x-lisp,text/x-python,text/x-h,text/x-php,text/x-shellscript,application/json,application/xml,application/javascript,application/x-httpd-php,text/css,text/html,text/csv,text/markdown'])
+            ->addRule(Form::MAX_FILE_SIZE, 'Maximum file size is 10 mb.', 10 * 1024 * 1024);
+
         $form->addSubmit('submit', T::trans('Create'));
-        
+
         if ($form->isSuccess()) {
             $values = $form->getValues();
-        
-        	if (isset($values['topic']) && isset($values['mainfilename'])) {
-                
-                $id = $this->pageModel->create($values['topic'],$values['mainfilename']);
-                
-        	    if(isset($id)) {
-            	    $this->pageModel->addPageData($id, $this->doc->valuesToArray(array('options' => 'title', 'option_content' => $values['mainfilename'])));
-            	    $this->doc->buildPhpPage($id);
-        
+            $uPath = 'pages' . DIRECTORY_SEPARATOR . $values['topic'] . DIRECTORY_SEPARATOR . $values['filename'] . '.php';
+            if (!file_exists($uPath)) {
+                $id = $this->pageModel->create($values['topic'], $values['filename']);
+                if(isset($id)) {
+                    $this->pageModel->addPageData($id, $this->doc->valuesToArray(array('options' => 'description', 'option_content' => $values['description'])));
+                    $file = $values['file'];
+                    $file_path = $this->doc->upload($file, $this->pageModel->getPhpPath($id));
+                    $this->pageModel->addPageData($id, $this->doc->valuesToArray($values, $file_path));
+                    $this->doc->buildPhpPage($id);
+
                     header('Location:'.$this->pageModel->getTopic($id).'/'.$this->pageModel->getFilename($id));
-        			exit;
-        	    } else {
+                    exit;
+                } else {
                     $this->msg->error(T::trans('Sorry something didn\'t work!'),BASE_URL.'page/create');
-        	    }
-        	}
+                }
+            }
         }
         return $form;
     }
 }
+
 
