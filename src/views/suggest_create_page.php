5d2286ceb611d27cf5d3d82951c50b7e931b1c5c
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-06-19 01:27:17 +0100

    Refine page creation workflow

---
+++ src/views/suggest_create_page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the DocPHT project.
+ *
+ * @author Valentino Pesce
+ * @copyright (c) Valentino Pesce <valentino@iltuobrand.it>
+ * @copyright (c) Craig Crosby <creecros@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+?>
+
+<div class="jumbotron text-center fade-in-fwd">
+    <h1><?= $t->trans('Page not found') ?></h1>
+    <p><?= $t->trans('The page you are looking for does not exist:') ?> <strong><?= htmlspecialchars($filename, ENT_QUOTES, 'UTF-8') ?></strong></p>
+    <p><?= $t->trans('You can create this page:') ?></p>
+    <a href="<?= BASE_URL ?>page/create?topic=<?= urlencode($topic) ?>&filename=<?= urlencode($filename) ?>" class="btn btn-primary"><?= $t->trans('Create Page') ?></a>
+    <hr>
+    <p><?= $t->trans('Alternatively, you can search for the page:') ?></p>
+    <form action="<?= BASE_URL ?>page/search" method="post">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" name="search" placeholder="<?= $t->trans('Search for...') ?>" value="<?= htmlspecialchars($filename, ENT_QUOTES, 'UTF-8') ?>">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="submit"><?= $t->trans('Search') ?></button>
+            </div>
+        </div>
+    </form>
+</div>
