1f3ea774ce95b995087a460fa0ac3282e3209fa0
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-07-06 01:28:18 +0800

    Switch search to grep

--- src/model/SearchModel.php
+++ src/model/SearchModel.php
@@ -21,21 +21,8 @@ class SearchModel extends PageModel
 {
     public function feed()
     {
-        $data = $this->getAllIndexed();
-        $array = [];
-        if ($data !== false) {
-            foreach ($data as $value) {
-                if (!empty($value['slug']) && !empty($value['topic'])) {
-                    $array[] = $this->add($value['slug'], $value['topic']);
-                    $array[] = $this->add($value['slug'], $value['filename']);
-                }
-                $markdown = $this->get($value['slug']);
-                if ($markdown !== null) {
-                    $array[] = $this->add($value['slug'], $markdown);
-                }
-            }
-        }
-        $this->disconnect('json/search.json', $array);
+        // Search indexing handled via grep; no JSON generation required.
+        return true;
     }
     
     public function add($slug, $content)
