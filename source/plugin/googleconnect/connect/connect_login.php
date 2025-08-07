e17b5658cf664ea50c96183a543d047f9713a8cf
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-08-07 09:36:58 -0500

    fix "id_token must be passed in or set as part of setAccessToken" error.

--- source/plugin/googleconnect/connect/connect_login.php
+++ source/plugin/googleconnect/connect/connect_login.php
@@ -18,16 +18,20 @@ if(!in_array($op, array('init', 'callback', 'change'))) {
 $referer = dreferer();
 $referer = $referer && (strpos($referer, 'mod=register') === false) && (strpos($referer, 'mod=login') === false) ? $referer : 'index.php';
 
+if (empty($_POST['credential'])) {
+	showmessage('Invalid ID token', $referer);
+}
+
 require_once 'vendor/autoload.php';
 $client = new Google_Client(['client_id' => $_G['setting']['connectappid']]);
 $payload = $client->verifyIdToken($_POST['credential']);
-if ($payload) {
-  $gmail = $payload['email'];
-  $username = $payload['name'];
-} else {
-  showmessage('Invalid ID token', $referer);
+
+if (!$payload) {
+	showmessage('Invalid ID token', $referer);
 }
 
+$gmail = $payload['email'];
+$username = $payload['name'];
 if($op == 'callback') {
 	global $_G;
 	
