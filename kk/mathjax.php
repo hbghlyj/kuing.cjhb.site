40f0f658711cd28dd4c4a1bc512c963abe3f9776
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-09-02 01:52:21 -0500

    MathJax v4 \perp 与两边元素间距不正确

--- kk/mathjax.php
+++ kk/mathjax.php
@@ -1,8 +1,20 @@
 <script src="/kk/mathjax-config.js?<?= $_G['style']['verhash']?>"></script>
 <script>
-  <?php if(!empty($_GET['highlight'])): ?>
   window.MathJax.startup = {
     ready: function() {
+      const { RANGES }   = MathJax._.core.MmlTree.OperatorDictionary;
+      const { TEXCLASS } = MathJax._.core.MmlTree.MmlNode;
+      function promote(codepoint, texClass, nodeType) {
+        const i = RANGES.findIndex(([a, b]) => a <= codepoint && codepoint <= b);
+        if (i < 0) return;
+        const [a, b, cls, node] = RANGES[i];
+        const before = (a <= codepoint - 1) ? [[a, codepoint - 1, cls, node]] : [];
+        const middle = [[codepoint, codepoint, texClass, nodeType]];
+        const after  = (codepoint + 1 <= b) ? [[codepoint + 1, b, cls, node]] : [];
+        RANGES.splice(i, 1, ...before, ...middle, ...after);
+      }
+      promote(0x27C2, TEXCLASS.REL, 'mo');
+      <?php if(!empty($_GET['highlight'])): ?>
       const {HTMLDomStrings} = MathJax._.handlers.html.HTMLDomStrings;
       HTMLDomStrings.OPTIONS.includeHtmlTags['mark'] = '';
       var handleTag = HTMLDomStrings.prototype.handleTag;
@@ -14,10 +26,10 @@
         }
         return handleTag.call(this, node, ignore);
       }
+      <?php endif; ?>
       MathJax.startup.defaultReady();
     }
   }
-  <?php endif; ?>
   window.MathJax.svg.scale = <?php if(defined("IN_MOBILE")) { ?>0.9<?php } else { ?>1.1<?php } ?>;
 </script>
 <script src="https://unpkg.com/mathjax@4.0.0/tex-svg.js" async></script>
