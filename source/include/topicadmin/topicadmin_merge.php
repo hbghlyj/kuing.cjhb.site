9b539d24a977f325a8431fb1eb786746c98445ec
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-09-15 07:55:34 +0100

    Add post ordering options to thread merge (#585)
    * Add merge post positioning options
    
    * style: restore tab indentation in merge handler
    
    * Refactor post fetch logic in merge handler

--- source/include/topicadmin/topicadmin_merge.php
+++ source/include/topicadmin/topicadmin_merge.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- *      [Discuz!] (C)2001-2099 Comsenz Inc.
- *      This is NOT a freeware, use is subject to license terms
- *
- *      $Id: topicadmin_merge.php 31741 2012-09-26 08:12:08Z zhangjie $
- */
+	*      [Discuz!] (C)2001-2099 Comsenz Inc.
+	*      This is NOT a freeware, use is subject to license terms
+	*
+	*      $Id: topicadmin_merge.php 31741 2012-09-26 08:12:08Z zhangjie $
+	*/
 
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
@@ -24,6 +24,7 @@ if(!submitcheck('modsubmit')) {
 	$posttable = getposttablebytid($_G['tid']);
 	$othertid = intval($_GET['othertid']);
 	$otherposttable = getposttablebytid($othertid);
+	$newpostposition = intval($_GET['newpostposition']);
 	$modaction = 'MRG';
 
 	$reason = checkreasonpm();
@@ -35,24 +36,24 @@ if(!submitcheck('modsubmit')) {
 		showmessage('special_noaction');
 	}
 if($othertid == $_G['tid'] || ($_G['adminid'] == 3 && $other['fid'] != $_G['forum']['fid'])) {
-        showmessage('admin_merge_invalid');
+	showmessage('admin_merge_invalid');
 }
 
 $thread = C::t('forum_thread')->fetch($_G['tid']);
 $taglist = array();
 foreach(array($thread['tags'], $other['tags']) as $tagstr) {
-    foreach(explode("\t", $tagstr) as $var) {
-        if($var) {
-            list($id, $name) = explode(',', $var);
-            $taglist[$id] = $name;
-            C::t('common_tagitem')->replace($id, $_G['tid'], 'tid');
-        }
-    }
+	foreach(explode("\t", $tagstr) as $var) {
+	if($var) {
+		list($id, $name) = explode(',', $var);
+		$taglist[$id] = $name;
+		C::t('common_tagitem')->replace($id, $_G['tid'], 'tid');
+	}
+	}
 }
 C::t('common_tagitem')->delete_tagitem(0, $othertid, 'tid');
 $newtagstr = '';
 foreach($taglist as $id => $name) {
-    $newtagstr .= $id.','.$name."\t";
+	$newtagstr .= $id.','.$name."\t";
 }
 C::t('forum_thread')->update($_G['tid'], array('tags' => $newtagstr));
 
@@ -64,40 +65,32 @@ C::t('forum_thread')->update($_G['tid'], array('tags' => $newtagstr));
 	if(!$thread['maxposition']) {
 		$thread['maxposition'] = C::t('forum_post')->fetch_maxposition_by_tid($thread['posttableid'], $_G['tid']);
 	}
-	$pos = 1;
+	$postlist1 = $postlist2 = array();
 	if($posttable != $otherposttable) {
-		$pidlist = array();
 		C::t('forum_post')->increase_position_by_tid($thread['posttableid'], $_G['tid'], $other['maxposition'] + $thread['maxposition']);
 		C::t('forum_post')->increase_position_by_tid($other['posttableid'], $othertid, $other['maxposition'] + $thread['maxposition']);
-		foreach(C::t('forum_post')->fetch_all_by_tid('tid:'.$_G['tid'], $_G['tid'], false, 'ASC') as $row) {
-			$pidlist[$row['dateline']] = array('pid' => $row['pid'], 'tid' => $row['tid']);
-		}
-		foreach(C::t('forum_post')->fetch_all_by_tid('tid:'.$othertid, $othertid, false, 'ASC') as $row) {
-			$pidlist[$row['dateline']] = array('pid' => $row['pid'], 'tid' => $row['tid']);
-		}
-		ksort($pidlist);
-		foreach($pidlist as $row) {
-			C::t('forum_post')->update_post('tid:'.$row['tid'], $row['pid'], array('position' => $pos));
-			$pos ++;
-		}
-		unset($pidlist);
 	} else {
 		C::t('forum_post')->increase_position_by_tid($thread['posttableid'], array($_G['tid'], $othertid), $other['maxposition'] + $thread['maxposition']);
-		foreach(C::t('forum_post')->fetch_all_by_tid('tid:'.$_G['tid'], array($_G['tid'], $othertid), false, 'ASC') as $row) {
-			C::t('forum_post')->update_post('tid:'.$_G['tid'], $row['pid'], array('position' => $pos));
-			$pos ++;
-		}
 	}
+	foreach(C::t('forum_post')->fetch_all_by_tid('tid:'.$_G['tid'], $_G['tid'], false, 'ASC') as $row) {
+		$postlist1[] = $row;
+	}
+	foreach(C::t('forum_post')->fetch_all_by_tid('tid:'.$othertid, $othertid, false, 'ASC') as $row) {
+		$postlist2[] = $row;
+	}
+	$pidlist = $newpostposition ? array_merge($postlist2, $postlist1) : array_merge($postlist1, $postlist2);
+	$pos = 1;
+	foreach($pidlist as $row) {
+		C::t('forum_post')->update_post('tid:'.$row['tid'], $row['pid'], array('position' => $pos));
+		$pos++;
+	}
+	$firstpost = reset($pidlist);
+	unset($postlist1, $postlist2, $pidlist);
 	if($posttable != $otherposttable) {
 		foreach(C::t('forum_post')->fetch_all_by_tid('tid:'.$othertid, $othertid) as $row) {
 			C::t('forum_post')->insert_post('tid:'.$_G['tid'], $row);
 		}
 		C::t('forum_post')->delete_by_tid('tid:'.$othertid, $othertid);
-	}
-
-	$query = C::t('forum_post')->fetch_all_by_tid('tid:'.$_G['tid'], array($_G['tid'], $othertid), false, 'ASC', 0, 1, null, 0);
-	foreach($query as $row) {
-		$firstpost = $row;
 	}
 
 	$postsmerged = C::t('forum_post')->update_by_tid('tid:'.$_G['tid'], $othertid, array('tid' => $_G['tid']));
@@ -133,11 +126,11 @@ C::t('forum_thread')->update($_G['tid'], array('tags' => $newtagstr));
 
 	$modpostsnum ++;
 	$resultarray = array(
-	'redirect'	=> "forum.php?mod=viewthread&tid=$_G[tid]",
-	'reasonpm'	=> ($sendreasonpm ? array('data' => array($thread), 'var' => 'thread', 'item' => 'reason_merge', 'notictype' => 'post') : array()),
-	'reasonvar'	=> array('tid' => $thread['tid'], 'subject' => $thread['subject'], 'modaction' => $modaction, 'reason' => $reason),
-	'modtids'	=> $thread['tid'],
-	'modlog'	=> array($thread, $other)
+	'redirect'      => "forum.php?mod=viewthread&tid=$_G[tid]",
+	'reasonpm'      => ($sendreasonpm ? array('data' => array($thread), 'var' => 'thread', 'item' => 'reason_merge', 'notictype' => 'post') : array()),
+	'reasonvar'     => array('tid' => $thread['tid'], 'subject' => $thread['subject'], 'modaction' => $modaction, 'reason' => $reason),
+	'modtids'       => $thread['tid'],
+	'modlog'        => array($thread, $other)
 	);
 
 }
