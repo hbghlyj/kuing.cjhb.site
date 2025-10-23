d4615f3d22cc5d9ba0a654dfb4a8cd50528595ea
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-10-22 22:25:31 -0500

    leftover of Commit 9f2edfe

--- static/js/bbcode.js
+++ static/js/bbcode.js
@@ -67,7 +67,7 @@ function bbcode2html(str) {
 				for(var page in smilies_array[typeid]) {
 					for(var i in smilies_array[typeid][page]) {
 						re = new RegExp(preg_quote(smilies_array[typeid][page][i][1]), "g");
-						str = str.replace(re, '<img src="' + STATICURL + 'image/smiley/' + smilies_type['_' + typeid][1] + '/' + smilies_array[typeid][page][i][2] + '" border="0" smilieid="' + smilies_array[typeid][page][i][0] + '" alt="' + smilies_array[typeid][page][i][1] + '" />');
+						str = str.replace(re, '<img src="' + STATICURL + 'image/smiley/' + smilies_array[typeid][page][i][2] + '" border="0" smilieid="' + smilies_array[typeid][page][i][0] + '" alt="' + smilies_array[typeid][page][i][1] + '" />');
 					}
 				}
 			}
