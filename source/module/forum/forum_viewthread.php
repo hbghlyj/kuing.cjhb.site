d574020744752a220b6108c8f608260d76265963
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2025-11-23 14:31:30 -0600

    Fix author reply detection with integer first flag (#599)

--- source/module/forum/forum_viewthread.php
+++ source/module/forum/forum_viewthread.php
@@ -662,7 +662,7 @@ if($hotpostarr || $sticklist) {
 foreach($postarr as $post) {
 	if(($onlyauthoradd && empty($post['anonymous']) || !$onlyauthoradd) && !isset($postlist[$post['pid']])) {
 
-		$post['authorself'] = false;
+		$post['authorself'] = $thread['authorid'] && $post['authorid'] == $thread['authorid'] && !$post['first'];
 
 		if(isset($hotpostarr[$post['pid']])) {
 			$post['existinfirstpage'] = true;
@@ -706,9 +706,6 @@ foreach($postarr as $post) {
 					$post['releatcollectionnum'] = 0;
 				}
 			}
-		}
-		if($thread['authorid'] && $post['authorid'] == $thread['authorid'] && $post['first'] !== '1') {
-			$post['authorself'] = true;
 		}
 		$postlist[$post['pid']] = $post;
 	}
