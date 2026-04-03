8039f65ab4684b7e31ef5d30040e3d2a727a144b
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-03 00:46:40 -0500

    fix: use bracket string offset in uc installer

--- upload/uc_server/install/index.php
+++ upload/uc_server/install/index.php
@@ -135,7 +135,7 @@ if($method == 'show_license') {
 		}
 
 		// Table prefixes must end with "_" to avoid names like "pre_1" breaking later parsing.
-		if(strpos($tablepre, '.') !== false || intval($tablepre{0}) || substr($tablepre, -1) !== '_') {
+		if(strpos($tablepre, '.') !== false || intval($tablepre[0]) || substr($tablepre, -1) !== '_') {
 			show_msg('tablepre_invalid', $tablepre, 0);
 		}
 
