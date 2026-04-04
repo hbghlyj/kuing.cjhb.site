266ff41265f7e3fbdb05fbe2056f3e0184c714af
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-04 15:15:47 -0500

    Normalize touch forum image fetch from upload tail

--- source/function/function_forumlist.php
+++ source/function/function_forumlist.php
@@ -498,7 +498,7 @@ function get_attach($list, $video = false, $audio = false) {
 	foreach($attachtableid_array as $tableid => $pids) {
 		$attachs = table_forum_attachment_n::t()->fetch_all_by_pid_width($tableid, $pids, 0);
 		foreach($attachs as $value) {
-			$threadlist_data[$value['tid']]['attachment'][] = getforumimg($value['aid'], 0, 550, 350);
+			$threadlist_data[$value['tid']]['attachment'][] = getforumimg($value['aid'], 0, 2000, 550);
 		}
 	}
 	return $threadlist_data;
