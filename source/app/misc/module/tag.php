6433f64c7478a1651dba707698292b39863ac8cb
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-05 08:57:22 -0500

    Align tag moderation panel with master

--- source/app/misc/module/tag.php
+++ source/app/misc/module/tag.php
@@ -16,28 +16,6 @@ if(!$op || in_array($op, ['list', 'detail'])) {
 	require_once libfile('misc/tag', 'module');
 	return;
 }elseif(in_array($_GET['op'], ['add', 'search', 'manage', 'set'])) {
-	global $_G;
-	$taglist = [];
-	if(intval($_GET['tid']) > 0) {
-		$postinfo = table_forum_post::t()->fetch_threadpost_by_tid_invisible(intval($_GET['tid']));
-		if($postinfo['tags']) {
-			$tagarray_all = $array_temp = $threadtag_array = [];
-			$tagarray_all = explode("\t", $postinfo['tags']);
-			if($tagarray_all) {
-				foreach($tagarray_all as $var) {
-					if($var) {
-						$array_temp = explode(',', $var);
-						$threadtag_array[] = $array_temp['1'];
-					}
-				}
-			}
-			$tags = implode(',', $threadtag_array);
-		}
-	}
-	$file = childfile($op);
-	if(file_exists($file)) {
-		require_once $file;
-	}
-
-	include_once template('tag/tagmanage');
+	require_once libfile('forum/tag', 'module');
+	return;
 }
