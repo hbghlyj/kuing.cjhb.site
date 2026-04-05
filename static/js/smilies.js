c1ed9a4fe1a83d2a5a5e21a7ae4d9d30bc9b765a
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-05 09:25:24 -0500

    Align smilies.js with master and fix icon size

--- static/js/smilies.js
+++ static/js/smilies.js
@@ -1,8 +1,9 @@
-/**
- * [Discuz!] (C)2001-2099 Discuz! Team
- * This is NOT a freeware, use is subject to license terms
- * https://license.discuz.vip
- */
+/*
+	[Discuz!] (C)2001-2099 Comsenz Inc.
+	This is NOT a freeware, use is subject to license terms
+
+	$Id: smilies.js 29684 2012-04-25 04:00:58Z zhangguosheng $
+*/
 
 function _smilies_show(id, smcols, seditorkey) {
 	if(seditorkey && !$(seditorkey + 'sml_menu')) {
@@ -20,11 +21,17 @@ function _smilies_show(id, smcols, seditorkey) {
 		var scriptNode = document.createElement("script");
 		scriptNode.type = "text/javascript";
 		scriptNode.charset = charset ? charset : (BROWSER.firefox ? document.characterSet : document.charset);
-		scriptNode.src = JSPATH + 'common_smilies_var.js?' + VERHASH;
+		scriptNode.src = 'data/cache/common_smilies_var.js?' + VERHASH;
 		$('append_parent').appendChild(scriptNode);
-		scriptNode.onload = function() {
-			smilies_onload(id, smcols, seditorkey);
-		};
+		if(BROWSER.ie) {
+			scriptNode.onreadystatechange = function() {
+				smilies_onload(id, smcols, seditorkey);
+			};
+		} else {
+			scriptNode.onload = function() {
+				smilies_onload(id, smcols, seditorkey);
+			};
+		}
 	} else {
 		smilies_onload(id, smcols, seditorkey);
 	}
@@ -32,25 +39,9 @@ function _smilies_show(id, smcols, seditorkey) {
 
 function smilies_onload(id, smcols, seditorkey) {
 	seditorkey = !seditorkey ? '' : seditorkey;
-	smile = getcookie('smile').split('D');
 	if(typeof smilies_type == 'object') {
-		if(smile[0] && smilies_array[smile[0]]) {
-			CURRENTSTYPE = smile[0];
-		} else {
-			for(i in smilies_array) {
-				CURRENTSTYPE = i;break;
-			}
-		}
-		smiliestype = '<div id="'+id+'_tb" class="tb tb_s cl"><ul>';
-		for(i in smilies_type) {
-			key = i.substring(1);
-			if(smilies_type[i][0]) {
-				smiliestype += '<li ' + (CURRENTSTYPE == key ? 'class="current"' : '') + ' id="'+seditorkey+'stype_'+key+'" onclick="smilies_switch(\'' + id + '\', \'' + smcols + '\', '+key+', 1, \'' + seditorkey + '\');if(CURRENTSTYPE) {$(\''+seditorkey+'stype_\'+CURRENTSTYPE).className=\'\';}this.className=\'current\';CURRENTSTYPE='+key+';doane(event);"><a href="javascript:;" hidefocus="true">'+smilies_type[i][0]+'</a></li>';
-			}
-		}
-		smiliestype += '</ul></div>';
-		$(id).innerHTML = smiliestype + '<div id="' + id + '_data"></div><div class="sllt_p" id="' + id + '_page"></div>';
-		smilies_switch(id, smcols, CURRENTSTYPE, smile[1], seditorkey);
+		$(id).innerHTML = '<div id="' + id + '_data"></div><div class="sllt_p" id="' + id + '_page"></div>';
+		smilies_switch(id, smcols, CURRENTSTYPE, 0, seditorkey);
 		smilies_fastdata = '';
 		if(seditorkey == 'fastpost' && $('fastsmilies') && smilies_fast) {
 			var j = 0;
@@ -60,59 +51,47 @@ function smilies_onload(id, smcols, seditorkey) {
 				}
 				j = ++j > 3 ? 0 : j;
 				s = smilies_array[smilies_fast[i][0]][smilies_fast[i][1]][smilies_fast[i][2]];
-				if(smilies_type['_' + smilies_fast[i][0]][1] == ':emoji') {
-					smilies_fastdata += s ? '<td onclick="' + (typeof wysiwyg != 'undefined' ? 'insertSmiley(' + s[0] + ')' : 'seditor_insertunit(\'' + seditorkey + '\', \'' + s[1] + '\')') +
-					'" id="' + seditorkey + 'smilie_' + s[0] + '_td"><span id="smilie_' + s[0] + '">' + s[1] + '</span>' : '<td>';
-				} else {
-					smilieimg = STATICURL + 'image/smiley/' + smilies_type['_' + smilies_fast[i][0]][1] + '/' + s[2];
-					img[k] = new Image();
-					img[k].src = smilieimg;
-					smilies_fastdata += s ? '<td onmouseover="smilies_preview(\'' + seditorkey + '\', \'fastsmiliesdiv\', this, ' + s[5] + ')" onmouseout="$(\'smilies_preview\').style.display = \'none\'" onclick="' + (typeof wysiwyg != 'undefined' ? 'insertSmiley(' + s[0] + ')' : 'seditor_insertunit(\'' + seditorkey + '\', \'' + s[1].replace(/'/, '\\\'') + '\')') +
-						'" id="' + seditorkey + 'smilie_' + s[0] + '_td"><img id="smilie_' + s[0] + '" width="' + s[3] + '" height="' + s[4] + '" src="' + smilieimg + '" alt="' + s[1] + '" />' : '<td>';
-				}
+				smilieimg = STATICURL + 'image/smiley/' + s[1];
+				img[k] = new Image();
+				img[k].src = smilieimg;
+				smilies_fastdata += s ? '<td onmouseover="smilies_preview(\'' + seditorkey + '\', \'fastsmiliesdiv\', this, ' + s[5] + ')" onmouseout="$(\'smilies_preview\').style.display = \'none\'" onclick="' + (typeof wysiwyg != 'undefined' ? 'insertSmiley(' + i + ')': 'seditor_insertunit(\'' + seditorkey + '\', \'' + s[0].replace(/'/, '\\\'') + '\')') +
+					'" id="' + seditorkey + 'smilie_' + i + '_td"><img id="smilie_' + i + '" width="20" height="20" src="' + smilieimg + '" alt="' + s[0] + '" />' : '<td>';
 			}
 			$('fastsmilies').innerHTML = '<table cellspacing="0" cellpadding="0"><tr>' + smilies_fastdata + '</tr></table>';
 		}
 	}
 }
 
-function smilies_switch(id, smcols, type, page, seditorkey) {
-	page = page ? page : 1;
-	if(!smilies_array[type] || !smilies_array[type][page]) return;
-	setcookie('smile', type + 'D' + page, 31536000);
+function smilies_switch(id, smcols, type, page = 0, seditorkey) {
 	smiliesdata = '<table id="' + id + '_table" cellpadding="0" cellspacing="0"><tr>';
 	j = k = 0;
 	img = [];
-	for(var i = 0; i < smilies_array[type][page].length; i++) {
+	for(var i = page * 40; i < smilies_array.length && i < (page + 1) * 40;j++,k++) {
+		i++;
 		if(j >= smcols) {
 			smiliesdata += '<tr>';
 			j = 0;
 		}
-		s = smilies_array[type][page][i];
-		smilieimg = STATICURL + 'image/smiley/' + smilies_type['_' + type][1] + '/' + s[2];
-		if(smilies_type['_' + type][1] == ':emoji') {
-			smiliesdata += s && s[0] ? '<td onclick="' + (typeof wysiwyg != 'undefined' ? 'insertSmiley(' + s[0] + ')' : 'seditor_insertunit(\'' + seditorkey + '\', \'' + s[1] + '\')') +
-			'" id="' + seditorkey + 'smilie_' + s[0] + '_td"><span id="smilie_' + s[0] + '">' + s[1] + '</span>' : '<td>';
-		} else {
-			img[k] = new Image();
-			img[k].src = smilieimg;
-			smiliesdata += s && s[0] ? '<td onmouseover="smilies_preview(\'' + seditorkey + '\', \'' + id + '\', this, ' + s[5] + ')" onclick="' + (typeof wysiwyg != 'undefined' ? 'insertSmiley(' + s[0] + ')' : 'seditor_insertunit(\'' + seditorkey + '\', \'' + s[1].replace(/'/, '\\\'') + '\')') +
-				'" id="' + seditorkey + 'smilie_' + s[0] + '_td"><img id="smilie_' + s[0] + '" width="' + s[3] + '" height="' + s[4] + '" src="' + smilieimg + '" alt="' + s[1] + '" />' : '<td>';
-		}
-		j++;k++;
+		s = smilies_array[i];
+		if(!s) continue;
+		smilieimg = STATICURL + 'image/smiley/' + s[1];
+		img[k] = new Image();
+		img[k].src = smilieimg;
+		smiliesdata += s && i ? '<td onmouseover="smilies_preview(\'' + seditorkey + '\', \'' + id + '\', this, ' + s[5] + ')" onclick="' + (typeof wysiwyg != 'undefined' ? 'insertSmiley(' + i + ')': 'seditor_insertunit(\'' + seditorkey + '\', \'' + s[0].replace(/'/, '\\\'') + '\')') +
+			'" id="' + seditorkey + 'smilie_' + i + '_td"><img id="smilie_' + i + '" width="20" height="20" src="' + smilieimg + '" alt="' + s[0] + '" />' : '<td>';
 	}
 	smiliesdata += '</table>';
 	smiliespage = '';
-	if(smilies_array[type].length > 2) {
-		prevpage = ((prevpage = parseInt(page) - 1) < 1) ? smilies_array[type].length - 1 : prevpage;
-		nextpage = ((nextpage = parseInt(page) + 1) == smilies_array[type].length) ? 1 : nextpage;
-		smiliespage = '<div class="z"><a href="javascript:;" onclick="smilies_switch(\'' + id + '\', \'' + smcols + '\', ' + type + ', ' + prevpage + ', \'' + seditorkey + '\');doane(event);">' + $L('prev_page_s') + '</a>' +
-			'<a href="javascript:;" onclick="smilies_switch(\'' + id + '\', \'' + smcols + '\', ' + type + ', ' + nextpage + ', \'' + seditorkey + '\');doane(event);">' + $L('next_page_s') + '</a></div>' +
-			page + '/' + (smilies_array[type].length - 1);
+	if(smilies_array.length > 40) {
+		prevpage = page - 1;
+		nextpage = page + 1;
+               smiliespage = '<div class="z">';
+			   if (prevpage >=0) smiliespage += '<a href="javascript:;" onclick="smilies_switch(\'' + id + '\', \'' + smcols + '\', ' + type + ', ' + prevpage + ', \'' + seditorkey + '\');doane(event);">'+lng['page_prev']+'</a>';
+			   if (nextpage < Math.ceil(smilies_array.length / 40)) smiliespage += '<a href="javascript:;" onclick="smilies_switch(\'' + id + '\', \'' + smcols + '\', ' + type + ', ' + nextpage + ', \'' + seditorkey + '\');doane(event);">'+lng['page_next']+'</a>';
+			   smiliespage += '</div>' + (page + 1) + '/' + Math.ceil(smilies_array.length / 40);
 	}
 	$(id + '_data').innerHTML = smiliesdata;
 	$(id + '_page').innerHTML = smiliespage;
-	$(id + '_tb').style.width = smcols*(16+parseInt(s[3])) + 'px';
 }
 
 function smilies_preview(seditorkey, id, obj, w) {
