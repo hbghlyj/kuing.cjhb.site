a0a69f02e848a18d5553d2f71da4a6af44c8ab6a
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-06 14:36:54 -0500

    Add English admin template language file

---
+++ source/i18n/EN_UTF8/admin/lang_template.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * [Discuz!] (C)2001-2099 Discuz! Team
+ * This is NOT a freeware, use is subject to license terms
+ * https://license.discuz.vip
+ */
+
+$lang = [
+	'stats_trend' => 'Overview',
+	'stats_more' => 'View all',
+	'stats_main_members' => 'Members',
+	'stats_main_today' => 'Today',
+	'stats_main_registers' => 'Registered',
+	'stats_main_ratio' => 'Ratio',
+	'stats_main_newposts' => 'New posts',
+	'stats_main_todaymembers' => 'New members today',
+	'stats_main_onlinemembers' => 'Online members',
+	'stats_main_posters' => 'Posting members',
+	'stats_main_posts' => 'Posts',
+
+	'ranklist_hotthread' => 'Recent hot threads',
+
+	'home_todo' => 'To-do',
+	'home_mod_verify_prefix' => 'Pending',
+	'home_mod_members' => 'Pending members',
+	'home_mod_threads' => 'Pending threads',
+	'home_mod_posts' => 'Pending replies',
+	'home_mod_medals' => 'Pending medals',
+	'home_mod_wait' => 'Pending groups',
+	'home_mod_blogs' => 'Pending blogs',
+	'home_mod_doings' => 'Pending doings',
+	'home_mod_pictures' => 'Pending pictures',
+	'home_mod_shares' => 'Pending shares',
+	'home_mod_comments' => 'Pending comments',
+	'home_mod_articles' => 'Pending articles',
+	'home_mod_articlecomments' => 'Pending article comments',
+	'home_mod_topiccomments' => 'Pending topic comments',
+	'home_mod_reports' => 'Pending reports',
+	'home_del_threads' => 'Recycle bin threads',
+	'home_err_credits' => 'Recent credit errors',
+
+	'main_nav' => 'Main navigation',
+	'quick_nav' => 'Quick navigation',
+
+	'reset' => 'Reset',
+	'back' => 'Back',
+	'refresh' => 'Refresh',
+	'favmgr' => 'Manage favorites',
+
+	'menu_update' => 'Menu updated',
+	'menu_update_close' => 'Close this notice. You can reset the menu later in Multi-platform Management.',
+	'menu_update_tips' => 'Click to update the menu',
+];
