939d942f9d96abf4fbb2302c9d55b28b58b34f5d
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-06 23:53:13 -0500

    Fix Google button rendering on full login pages

--- source/plugin/googleconnect/connect.class.php
+++ source/plugin/googleconnect/connect.class.php
@@ -3,9 +3,17 @@ if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 class plugin_googleconnect {
-	public function global_login_extra() {
+	private function buildGoogleMarkup() {
 		global $_G;
-		$googleButton = '<div class="g_id_signin"
+		return '<script src="https://accounts.google.com/gsi/client" async></script>
+		<div id="g_id_onload"
+			data-client_id="'.$_G['setting']['connectappid'].'"
+			data-context="signin"
+			data-login_uri="connect.php?mod=login&op=callback"
+			data-auto_select="true"
+			data-itp_support="true">
+		</div>
+		<div class="g_id_signin"
 data-type="standard"
 data-shape="pill"
 data-theme="filled_blue"
@@ -14,16 +22,13 @@ data-size="large"
 data-logo_alignment="left"
      data-width="40">
 </div>';
-		$_G['setting']['pluginhooks']['logging_method'] = ($_G['setting']['pluginhooks']['logging_method'] ?? '').$googleButton;
-		$_G['setting']['pluginhooks']['register_logging_method'] = ($_G['setting']['pluginhooks']['register_logging_method'] ?? '').$googleButton;
-		$_G['setting']['pluginhooks']['logging_method'] .= '<script src="https://accounts.google.com/gsi/client" async></script>';
-		return '<script src="https://accounts.google.com/gsi/client" async></script>
-		<div id="g_id_onload"
-			data-client_id="'.$_G['setting']['connectappid'].'"
-			data-context="signin"
-			data-login_uri="connect.php?mod=login&op=callback"
-			data-auto_select="true"
-			data-itp_support="true">
-		</div>';
+	}
+
+	public function global_login_extra() {
+		global $_G;
+		$googleMarkup = $this->buildGoogleMarkup();
+		$_G['setting']['pluginhooks']['logging_method'] = ($_G['setting']['pluginhooks']['logging_method'] ?? '').$googleMarkup;
+		$_G['setting']['pluginhooks']['register_logging_method'] = ($_G['setting']['pluginhooks']['register_logging_method'] ?? '').$googleMarkup;
+		return $googleMarkup;
 	}
 }
