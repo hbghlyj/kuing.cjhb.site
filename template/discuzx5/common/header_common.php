57d3963aff449658003b06a9d7a4933409ef11cf
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-07 02:34:43 -0500

    Style X5 Algolia input like original search box

--- template/discuzx5/common/header_common.php
+++ template/discuzx5/common/header_common.php
@@ -74,6 +74,7 @@
 	.dz_btm_layer{width: 1200px;margin: 0 auto;}
 	.dz_btm_layer .dz_layer_nav{width: 609px;}
 	.dz_btm_layer .header-searcher .search-input,
+	.dz_btm_layer .header-searcher .ais-SearchBox-input,
 	.dz_btm_layer .header-searcher #algolia-search-box:empty{width: 200px;}
 	<!--{/if}-->
 	<!--{if $_G['style']['top_nav_bgc']}-->
