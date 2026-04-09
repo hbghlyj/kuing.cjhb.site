0124d293219d770b9fa619fd5710174f333eca91
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-08 22:39:40 -0500

    Omit base tag on viewthread pages

--- template/default/touch/common/header_common.php
+++ template/default/touch/common/header_common.php
@@ -6,5 +6,5 @@
 	$_G['setting']['seohead_mobile']
 	{cells common/header/meta}
 	<!--{hook/global_meta_mobile}-->
-	<base href="{$_G['siteurl']}" />
+	<!--{if $_G['mod'] != 'viewthread'}--><base href="{$_G['siteurl']}" /><!--{/if}-->
 	<script type="text/javascript">var STYLEID = '{STYLEID}', STATICURL = '{STATICURL}', IMGDIR = '{IMGDIR}', VERHASH = '{VERHASH}', charset = '{CHARSET}', discuz_uid = '{$_G['uid']}', cookiepre = '{$_G['config']['cookie']['cookiepre']}', cookiedomain = '{$_G['config']['cookie']['cookiedomain']}', cookiepath = '{$_G['config']['cookie']['cookiepath']}', showusercard = '{$_G['setting']['showusercard']}', attackevasive = '{$_G['config']['security']['attackevasive']}', disallowfloat = '{$_G['setting']['disallowfloat']}', creditnotice = '<!--{if $_G['setting']['creditnotice']}-->$_G['setting']['creditnames']<!--{/if}-->', defaultstyle = '$_G['style']['defaultextstyle']', REPORTURL = '$_G['currenturl_encode']', SITEURL = '$_G['siteurl']', JSPATH = '$_G['setting']['jspath']', DEFAULTAVATAR = '$_G[setting][defaultavatar]';;</script>
