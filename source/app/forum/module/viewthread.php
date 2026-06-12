2f538f35363de410065253337ab7725054801ea0
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-06-12 09:56:50 +0800

    Use avatar status in thread post processing
    Remove filesystem-style noavatar URL probes and generate post avatar markup directly when avatarstatus is set. Apply the same logic to normal and stickied posts.

--- source/app/forum/module/viewthread.php
+++ source/app/forum/module/viewthread.php
@@ -404,9 +404,7 @@ if(empty($_GET['viewpid'])) {
 				continue;
 			}
 			$post['position'] = $poststick[$post['pid']]['position'];
-			$post['avatar'] = avatar($post['authorid'], 'small', true);
-			$post['avatar'] = strpos($post['avatar'], 'noavatar.') === false ? avatar($post['authorid'], 'small') : '';
-			empty($post['avatar']) && $post['avatarstatus'] = 0;
+			$post['avatar'] = $post['avatarstatus'] ? avatar($post['authorid'], 'small') : '';
 			$post['isstick'] = true;
 			$sticklist[$post['pid']] = $post;
 		}
@@ -1003,9 +1001,7 @@ function viewthread_procpost($post, $lastvisit, $ordertype, $maxposition = 0) {
 			}
 		}
 
-		$post['avatar'] = avatar($post['authorid'], 'middle', true);
-		$post['avatar'] = strpos($post['avatar'], 'noavatar.') === false ? avatar($post['authorid']) : '';
-		empty($post['avatar']) && $post['avatarstatus'] = 0;
+		$post['avatar'] = $post['avatarstatus'] ? avatar($post['authorid']) : '';
 		$post['groupicon'] = $post['avatar'] ? g_icon($post['groupid'], 1) : '';
 		$post['banned'] = $post['status'] & 1;
 		$post['warned'] = ($post['status'] & 2) >> 1;
