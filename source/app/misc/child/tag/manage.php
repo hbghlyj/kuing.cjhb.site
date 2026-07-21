bae263492d7a076c7a893f59255f843328dea74e
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-07-21 23:38:38 +0800

    Scope moderator recent tags to current forum
    Reuse the forum-aware recent tag helper in the thread tag moderation panel so it matches the posting form's filtering, ordering, and limit.

--- source/app/misc/child/tag/manage.php
+++ source/app/misc/child/tag/manage.php
@@ -25,22 +25,5 @@ if($_G['tid']) {
 	}
 	$tags = implode(',', $threadtag_array);
 
-	$recent_use_tag = [];
-	$i = 0;
-	$query = table_common_tagitem::t()->select(0, 0, 'tid', 'itemid', 'DESC', 10);
-	foreach($query as $result) {
-		if($i > 4) {
-			break;
-		}
-		if($recent_use_tag[$result['tagid']] == '') {
-			$i++;
-		}
-		$recent_use_tag[$result['tagid']] = 1;
-	}
-	if($recent_use_tag) {
-		$query = table_common_tag::t()->fetch_all(array_keys($recent_use_tag));
-		foreach($query as $result) {
-			$recent_use_tag[$result['tagid']] = $result['tagname'];
-		}
-	}
+	$recent_use_tag = recent_use_tag($_G['thread']['fid']);
 }
