7de6c439f81c5547758f83417db24c17049da5df
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-07-23 13:10:44 +0800

    remove 暂停预览 and 清空 buttons

--- kk/mathjax-preview.js
+++ kk/mathjax-preview.js
@@ -210,8 +210,6 @@ function renderFastTexSmilies() {
 
 // 侧边栏按钮列表
 var ctrls = [[
-	{ "n":"暂停预览", "o":"this.innerHTML=((input.yl = !input.yl) ? (input.oninput && input.oninput(), '暂停') : '继续')+'预览'" },
-	{ "n":"清空", "o":"if(input.value !== undefined){input.tmp_input=input.value;input.value='';input.oninput && input.oninput();}" },
 	{ "n":"撤销", "o":"if(input.value !== undefined){input.value=input.tmp_input || '';input.oninput && input.oninput();}" },
 	{ "n":"{}", "o":"if(input.setSelectionRange){input.setSelectionRange((function(){let brace=-1,i=input.selectionStart;do{switch(input.value[--i]){case '{':brace++;break;case '}':brace--;}}while(brace!=0&&i>0)return i})(),(function(){let brace=1,i=input.selectionEnd;do{switch(input.value[i++]){case '{':brace++;break;case '}':brace--;}}while(brace!=0&&i<input.value.length)return i})());input.focus();}" }
 ],[],['align*','gather*','cases'].map(v=>{ return{"n":v,"o":'input.cha(["\\\\begin{'+v+'}\\n","\\n\\\\end{'+v+'}",0,0])'} })];
