f351e634a73a268ac28a6d30b578491fc53bb133
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-07-25 00:25:26 +0800

    test: set allowcommentpost to 3 for usergroups 10 and 7 to enable first floor commenting

--- tools/test_forum.js
+++ tools/test_forum.js
@@ -138,8 +138,8 @@ const { execSync } = require('child_process');
         C::t('common_setting')->update('floodctrl', '0');
         C::t('common_setting')->update('pmstatus', '1');
         C::t('common_setting')->update('commentpostself', '1');
-        C::t('common_usergroup_field')->update(10, array('allowpostattach' => '1', 'allowpostimage' => '1', 'allowposttag' => '1', 'attachextensions' => 'gif, jpg, png, txt'));
-        C::t('common_usergroup_field')->update(7, array('allowpostattach' => '1', 'allowpostimage' => '1', 'allowposttag' => '1', 'attachextensions' => 'gif, jpg, png, txt'));
+        C::t('common_usergroup_field')->update(10, array('allowpostattach' => '1', 'allowpostimage' => '1', 'allowposttag' => '1', 'allowcommentpost' => '3', 'attachextensions' => 'gif, jpg, png, txt'));
+        C::t('common_usergroup_field')->update(7, array('allowpostattach' => '1', 'allowpostimage' => '1', 'allowposttag' => '1', 'allowcommentpost' => '3', 'attachextensions' => 'gif, jpg, png, txt'));
 
         require_once libfile('function/cache');
         updatecache(array('setting', 'secqaa', 'styles', 'usergroups'));
