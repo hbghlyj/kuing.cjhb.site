06f3e05925320e3831da966a58b6f3332414f71a
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-07-25 23:16:59 +0800

    fix(upload): load WebUploader dependency in X5 overlay

--- template/discuzx5/common/upload.php
+++ template/discuzx5/common/upload.php
@@ -1,6 +1,7 @@
 <?php exit('Access Denied');?>
 <!--{if empty($_G['uploadjs'])}-->
 	<link rel="stylesheet" type="text/css" href="{STATICURL}js/webuploader/webuploader.css?{VERHASH}">
+	<script src="{STATICURL}js/mobile/jquery.min.js?{VERHASH}"></script><script>jQuery.noConflict();</script>
 	<script src="{STATICURL}js/webuploader/webuploader.js?{VERHASH}"></script>
 	<script type="text/javascript" src="{$_G[setting][jspath]}discuz_uploader.js?{VERHASH}"></script>
 	{eval $_G['uploadjs'] = 1;}
