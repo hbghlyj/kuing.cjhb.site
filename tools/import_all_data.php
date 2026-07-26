55f1629137bdd96551ae7bb51c46b945b4ebe1f3
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-07-26 15:17:36 +0800

    test: define installer root path

--- tools/import_all_data.php
+++ tools/import_all_data.php
@@ -53,6 +53,7 @@ foreach(glob($dir.'/*.php') as $file) {
 
 // The installer writes serialized defaults after importing the language data.
 require_once './source/discuz_version.php';
+defined('ROOT_PATH') || define('ROOT_PATH', DISCUZ_ROOT);
 require './install/include/install_var.php';
 C::t('common_setting')->update('profilegroup', $serialize_sql_setting['profilegroup']);
 
