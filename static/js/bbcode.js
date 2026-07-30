c979d07ef584bf861e3e2dd0ea5b9941c844be7f
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-07-31 00:56:58 +0800

    fix(js): update bbcode2html backtick regex to preserve linebreaks matching PHP discuzcode parser

--- static/js/bbcode.js
+++ static/js/bbcode.js
@@ -104,7 +104,7 @@ function bbcode2html(str) {
 		str = str.replace(/\[float=right\]/ig, '<br style="clear: both"><span style="float: right; margin-left: 5px;">');
 		if(parsetype != 1) {
 			str = str.replace(/\[quote]([\s\S]*?)\[\/quote\]\s?\s?/ig, '<div class="quote"><blockquote>$1</blockquote></div>\n');
-			str = str.replace(/`([^`\r\n]+)`/g, '<code>$1</code>');
+			str = str.replace(/`([^`]+)`/g, '<code>$1</code>');
 		}
 
 		re = /\[table(?:=(\d{1,4}%?)(?:,([\(\)%,#\w ]+))?)?\]\s*([\s\S]+?)\s*\[\/table\]/ig;
