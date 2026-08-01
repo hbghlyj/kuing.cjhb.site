323e5cbdbb9bb9d57236fddef9873c2b3f73a9ce
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-08-01 15:09:27 -0500

    Revert "🛡️ Sentinel: [HIGH] Fix Stored Cross-Site Scripting (XSS) (#663)"
    This reverts commit 67e92a98151c2a928b37d0aa62520d5397687f56.

--- source/app/forum/extend/extend_thread_comment.php
+++ source/app/forum/extend/extend_thread_comment.php
@@ -21,23 +21,13 @@ class extend_thread_comment extends extend_thread_base {
 	private $postcomment;
 
 	public function before_newreply($parameters) {
-		require_once libfile('function/post');
 		list(, $this->param['modnewreplies']) = threadmodstatus($this->param['subject']."\t".$this->param['message'].$this->param['extramessage']);
 		if($this->thread['displayorder'] == -4) {
 			$this->param['modnewreplies'] = 0;
 		}
 		$pinvisible = $parameters['modnewreplies'] ? -2 : ($this->thread['displayorder'] == -4 ? -3 : 0);
 		$replypost = !empty($_GET['reppid']) ? table_forum_post::t()->fetch_post('tid:'.$this->thread['tid'], intval($_GET['reppid'])) : [];
-		// ✅ GOOD: HTML-escape and censor comment text before inserting into database to prevent Stored XSS
-		$postcomment = is_array($this->setting['allowpostcomment']) &&
-			in_array(2, $this->setting['allowpostcomment']) &&
-			$this->group['allowcommentreply'] &&
-			!$pinvisible &&
-			$replypost &&
-			($replypost['authorid'] != $this->member['uid'] || $this->setting['commentpostself'])
-			? censor(trim(dhtmlspecialchars(messagecutstr($parameters['message'], 200, ' '))), '***')
-			: '';
-		$this->postcomment = strlen($postcomment) > 255 ? '' : $postcomment;
+		$this->postcomment = is_array($this->setting['allowpostcomment']) && in_array(2, $this->setting['allowpostcomment']) && $this->group['allowcommentreply'] && !$pinvisible && $replypost && ($replypost['authorid'] != $this->member['uid'] || $this->setting['commentpostself']) ? messagecutstr($parameters['message'], 200, ' ') : '';
 	}
 
 	public function after_newreply() {
