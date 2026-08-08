b56432731e9eded9ee0d45be28ab1684b8fecf15
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-08-09 04:04:06 +0800

    fix(mathjax): add defensive property checks inside ready startup hook

--- kk/mathjax.php
+++ kk/mathjax.php
@@ -5,48 +5,67 @@
   window.MathJax.startup = {
     ready: function() {
       try {
-        const {MmlMath} = MathJax._.core.MmlTree.MmlNodes.math;
-        const {MmlMstyle} = MathJax._.core.MmlTree.MmlNodes.mstyle;
-        const {FONTSIZE} = MathJax._.output.chtml.Wrapper;
-        const {CHTML} = MathJax._.output.chtml_ts;
-        MmlMath.defaults.scriptminsize = '0px';
-        MmlMath.defaults.scriptsizemultiplier = 0.8;
-        MmlMstyle.defaults.scriptminsize = '0px';
-        MmlMstyle.defaults.scriptsizemultiplier = 0.8;
-        delete FONTSIZE['70.7%'];
-        delete FONTSIZE['70%'];
-        delete FONTSIZE['50%'];
-        FONTSIZE['80%'] = 's';
-        FONTSIZE['64%'] = 'ss';
-        CHTML.commonStyles['mjx-container [size="s"]']['font-size'] = '80%';
-        CHTML.commonStyles['mjx-container [size="ss"]']['font-size'] = '64%';
-        <?php if(!empty($_GET['highlight'])): ?>
-          const {HTMLDomStrings} = MathJax._.handlers.html.HTMLDomStrings;
-          HTMLDomStrings.OPTIONS.includeHtmlTags['mark'] = '';
-          var handleTag = HTMLDomStrings.prototype.handleTag;
-          HTMLDomStrings.prototype.handleTag = function (node, ignore) {
-            if (this.adaptor.kind(node) === 'mark') {
-            const text = this.adaptor.textContent(node);
-            this.snodes.push([node, text.length]);
-            this.string += text;
+        if (typeof MathJax !== 'undefined' && MathJax._ && MathJax._.core && MathJax._.output) {
+          const {MmlMath} = MathJax._.core.MmlTree.MmlNodes.math;
+          const {MmlMstyle} = MathJax._.core.MmlTree.MmlNodes.mstyle;
+          const {FONTSIZE} = MathJax._.output.chtml.Wrapper;
+          const {CHTML} = MathJax._.output.chtml_ts;
+          if (MmlMath && MmlMath.defaults) {
+            MmlMath.defaults.scriptminsize = '0px';
+            MmlMath.defaults.scriptsizemultiplier = 0.8;
           }
-          return handleTag.call(this, node, ignore);
+          if (MmlMstyle && MmlMstyle.defaults) {
+            MmlMstyle.defaults.scriptminsize = '0px';
+            MmlMstyle.defaults.scriptsizemultiplier = 0.8;
+          }
+          if (FONTSIZE) {
+            delete FONTSIZE['70.7%'];
+            delete FONTSIZE['70%'];
+            delete FONTSIZE['50%'];
+            FONTSIZE['80%'] = 's';
+            FONTSIZE['64%'] = 'ss';
+          }
+          if (CHTML && CHTML.commonStyles) {
+            if (CHTML.commonStyles['mjx-container [size="s"]']) CHTML.commonStyles['mjx-container [size="s"]']['font-size'] = '80%';
+            if (CHTML.commonStyles['mjx-container [size="ss"]']) CHTML.commonStyles['mjx-container [size="ss"]']['font-size'] = '64%';
+          }
+          <?php if(!empty($_GET['highlight'])): ?>
+            if (MathJax._.handlers && MathJax._.handlers.html) {
+              const {HTMLDomStrings} = MathJax._.handlers.html.HTMLDomStrings;
+              HTMLDomStrings.OPTIONS.includeHtmlTags['mark'] = '';
+              var handleTag = HTMLDomStrings.prototype.handleTag;
+              HTMLDomStrings.prototype.handleTag = function (node, ignore) {
+                if (this.adaptor.kind(node) === 'mark') {
+                  const text = this.adaptor.textContent(node);
+                  this.snodes.push([node, text.length]);
+                  this.string += text;
+                }
+                return handleTag.call(this, node, ignore);
+              };
+            }
+          <?php endif; ?>
+          if (MathJax._.input && MathJax._.input.tex) {
+            const { TEXCLASS } = MathJax._.core.MmlTree.MmlNode;
+            const Macro = MathJax._.input.tex.Token.Macro;
+            const {MakeBig} = MathJax._.input.tex.base.BaseMethods.default;
+            const mapHandler = MathJax._.input.tex.MapHandler.MapHandler.getMap('macros');
+            if (mapHandler && mapHandler.map && typeof mapHandler.map.set === 'function') {
+              mapHandler.map.set('big',new Macro('big', MakeBig, [TEXCLASS.ORD, .84]));
+              mapHandler.map.set('bigl',new Macro('bigl', MakeBig, [TEXCLASS.OPEN, .84]));
+              mapHandler.map.set('bigr',new Macro('bigr', MakeBig, [TEXCLASS.CLOSE, .84]));
+              mapHandler.map.set('bigm',new Macro('bigm', MakeBig, [TEXCLASS.REL, .84]));
+              mapHandler.map.set('bigg',new Macro('bigg', MakeBig, [TEXCLASS.ORD, 1.6]));
+              mapHandler.map.set('biggl',new Macro('biggl', MakeBig, [TEXCLASS.OPEN, 1.6]));
+              mapHandler.map.set('biggr',new Macro('biggr', MakeBig, [TEXCLASS.CLOSE, 1.6]));
+              mapHandler.map.set('biggm',new Macro('biggm', MakeBig, [TEXCLASS.REL, 1.6]));
+            }
+          }
         }
-        <?php endif; ?>
-        const { TEXCLASS } = MathJax._.core.MmlTree.MmlNode;
-        const Macro = MathJax._.input.tex.Token.Macro;
-        const {MakeBig} = MathJax._.input.tex.base.BaseMethods.default;
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('big',new Macro('big', MakeBig, [TEXCLASS.ORD, .84]));
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('bigl',new Macro('bigl', MakeBig, [TEXCLASS.OPEN, .84]));
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('bigr',new Macro('bigr', MakeBig, [TEXCLASS.CLOSE, .84]));
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('bigm',new Macro('bigm', MakeBig, [TEXCLASS.REL, .84]));
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('bigg',new Macro('bigg', MakeBig, [TEXCLASS.ORD, 1.6]));
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('biggl',new Macro('biggl', MakeBig, [TEXCLASS.OPEN, 1.6]));
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('biggr',new Macro('biggr', MakeBig, [TEXCLASS.CLOSE, 1.6]));
-        MathJax._.input.tex.MapHandler.MapHandler.getMap('macros').map.set('biggm',new Macro('biggm', MakeBig, [TEXCLASS.REL, 1.6]));
       } catch(e) {}
       if (MathJax && MathJax.startup && typeof MathJax.startup.defaultReady === 'function') {
-        MathJax.startup.defaultReady();
+        try {
+          MathJax.startup.defaultReady();
+        } catch(e) {}
       }
     }
   }
