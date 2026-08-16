3f4a84208682ede4c2749c1904793d42f9658002
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-08-16 15:45:35 +0800

    feat(status): show incognito icon for stealth mode

--- source/app/member/module/switchstatus.php
+++ source/app/member/module/switchstatus.php
@@ -25,8 +25,9 @@ if($_G['uid']) {
 		dsetcookie('ulastactivity', TIMESTAMP.'|'.getuserprofile('invisible'), 31536000);
 	}
 	$language = lang('forum/misc');
+	$icon = $_G['session']['invisible'] ? '<svg class="stealth-status-icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><circle cx="12" cy="8" r="3"></circle><path d="M5 21c.7-4 2.8-6 7-6s6.3 2 7 6M4 9h16M7 9l1.5 3h2L12 9l1.5 3h2L17 9"></path></svg>' : '';
 	$msg = $_G['session']['invisible'] ? $language['login_invisible_mode'] : $language['login_normal_mode'];
-	showmessage('<a href="member.php?mod=switchstatus" title="'.$language['login_switch_invisible_mode'].'" onclick="ajaxget(this.href, \'loginstatus\');return false;" class="xi2">'.$msg.'</a>', dreferer(), [], ['msgtype' => 3, 'showmsg' => 1]);
+	showmessage('<a href="member.php?mod=switchstatus" title="'.$language['login_switch_invisible_mode'].'" onclick="ajaxget(this.href, \'loginstatus\');return false;" class="xi2'.($_G['session']['invisible'] ? ' stealth-status' : '').'">'.$icon.$msg.'</a>', dreferer(), [], ['msgtype' => 3, 'showmsg' => 1]);
 
 }
 
