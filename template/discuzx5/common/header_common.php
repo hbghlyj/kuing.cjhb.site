ed3948a77e7e2c2289ebf8892466f000139bb1d0
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-08-24 08:09:38 -0500

    perf(mathjax): skip rendering for robots

--- template/discuzx5/common/header_common.php
+++ template/discuzx5/common/header_common.php
@@ -4,7 +4,7 @@
 	<head>
 	<title><!--{if !empty($navtitle)}-->$navtitle - <!--{/if}--><!--{if empty($nobbname)}--> $_G['setting']['bbname']<!--{/if}--></title>
 	<!--{hook/global_meta}-->
-	<!--{subtemplate common/header_mathjax}-->
+	<!--{if !IS_ROBOT}--><!--{subtemplate common/header_mathjax}--><!--{/if}-->
 	<!--{csstemplate}-->
 	<script>var STYLEID = '{STYLEID}', STATICURL = '{STATICURL}', IMGDIR = '{IMGDIR}', VERHASH = '{VERHASH}', FORMHASH = '{FORMHASH}', charset = '{CHARSET}', discuz_uid = '$_G[uid]', cookiepre = '{$_G[config][cookie][cookiepre]}', cookiedomain = '{$_G[config][cookie][cookiedomain]}', cookiepath = '{$_G[config][cookie][cookiepath]}', showusercard = '{$_G[setting][showusercard]}', attackevasive = '{$_G[config][security][attackevasive]}', disallowfloat = '{$_G[setting][disallowfloat]}', creditnotice = '<!--{if $_G['setting']['creditnotice']}-->$_G['setting']['creditnames']<!--{/if}-->', defaultstyle = '$_G[style][defaultextstyle]', REPORTURL = '$_G[currenturl_encode]', SITEURL = '$_G[siteurl]', JSCACHEPATH = '{$_G[setting][jscachepath]}', JSPATH = '$_G[setting][jspath]', CSSPATH = '$_G[setting][csspath]', DYNAMICURL = '{$_G[dynamicurl] or ''}', DISCUZ_I18N = '{echo currentlang();}', AVATARURL = '$_G[setting][avatarbase]';</script>
 	<script>
