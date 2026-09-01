d5db5c916cf0eba8b5067e68ec0ef9608ed4a09b
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-09-01 18:51:31 -0500

    style(search): unify Algolia input styling

--- template/discuzx5/common/header_common.php
+++ template/discuzx5/common/header_common.php
@@ -23,7 +23,6 @@
 	<style>
 	<!--{if !$_G['style']['top_nav_widthauto']}-->
 	.dz_btm_layer .dz_layer_nav{width: 609px;}
-	.dz_btm_layer .header-searcher .search-input,
 	.dz_btm_layer .header-searcher .ais-SearchBox-input,
 	.dz_btm_layer .header-searcher #algolia-search-box:empty{width: 200px !important;}
 	<!--{/if}-->
