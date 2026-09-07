f1e5834f997c918e3e727b63a863635087896ef8
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-09-07 13:39:16 -0500

    fix(i18n): clarify private messages label

--- source/i18n/EN/lang_template.php
+++ source/i18n/EN/lang_template.php
@@ -83,7 +83,7 @@ $lang = [
 	'my_nav' => 'Quick Navigation',
 	'my_nav_login' => 'Please <a href="javascript:;" class="xi2" onclick="lsSubmit()"><strong>Login</strong></a> to use quick navigation<br />No account? <a href="member.php?mod={$_G[\'setting\'][\'regname\']}" class="xi2 xw1">$_G[\'setting\'][\'reglinkname\']</a>',
 	'open_diy' => 'Open DIY Panel',
-	'pm_center' => 'Messages',
+	'pm_center' => 'Private Messages',
 	'portal_block_manage' => 'Module Management',
 	'portal_manage' => 'Portal Management',
 	'remind' => 'Notice',
