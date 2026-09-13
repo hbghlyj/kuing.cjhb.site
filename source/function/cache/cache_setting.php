f60c9ce7b6d5ec178b81a3612120e9044bf54107
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-09-13 04:04:48 -0500

    fix(i18n): localize nav logo alt per cache locale

--- source/function/cache/cache_setting.php
+++ source/function/cache/cache_setting.php
@@ -1005,7 +1005,7 @@ function get_cachedata_mainnav($locale = '') {
 
 		if($nav['logo']) {
 			$navlogo = admin\class_attach::getUrl($nav['logo']);
-			$navlogoalt = i18n::localizeValue($_G['setting']['bbname_i18n'] ?? $_G['setting']['bbname']);
+			$navlogoalt = i18n::localizeValue($_G['setting']['bbname_i18n'] ?? $_G['setting']['bbname'], $locale);
 			$data['navlogos'][$navid] = '<a href="'.$nav['url'].'" title="'.$navlogoalt.'"><img src="'.$navlogo.'" alt="'.$navlogoalt.'" border="0"></a>';
 		}
 
