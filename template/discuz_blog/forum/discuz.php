abb550f4a93b23db38decae7ba93a1807ca30deb
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-09-25 21:28:27 -0500

    Fix blog forum-link category rendering
    The forumlinks cache now contains grouped arrays, but the blog homepage still printed its former scalar slots. This rendered PHP's Array text instead of the links. Iterate each category and render its content, logo, and text sections like the default and X5 templates; add compact blog styles for group headings and counts.

--- template/discuz_blog/forum/discuz.php
+++ template/discuz_blog/forum/discuz.php
@@ -561,22 +561,19 @@
 			</div>
 		<!--{/if}-->
 
-		<!--{if empty($gid) && ($_G['cache']['forumlinks'][0] || $_G['cache']['forumlinks'][1] || $_G['cache']['forumlinks'][2])}-->
+		<!--{if empty($gid) && !empty($_G['cache']['forumlinks'])}-->
 		<div class="bm lk">
 			<div id="category_lk" class="bm_c ptm">
-				<!--{if $_G['cache']['forumlinks'][0]}-->
-					<ul class="m mbn cl">$_G['cache']['forumlinks'][0]</ul>
+				<!--{loop $_G['cache']['forumlinks'] $catid $cat}-->
+				<!--{if is_array($cat)}-->
+				<details class="lk_cat"<!--{if $catid == 1}--> open<!--{/if}-->>
+					<summary class="lk_cat_hd"><span class="lk_cat_name"><!--{eval echo lang('forum/template', 'friendlink_group'.$catid);}--></span><span class="lk_cat_count">($cat[count])</span></summary>
+					<!--{if is_string($cat[content]) && $cat[content]}--><ul class="m mbn cl">$cat[content]</ul><!--{/if}-->
+					<!--{if is_string($cat[logo]) && $cat[logo]}--><div class="mbn cl">$cat[logo]</div><!--{/if}-->
+					<!--{if is_string($cat[text]) && $cat[text]}--><ul class="x mbm cl">$cat[text]</ul><!--{/if}-->
+				</details>
 				<!--{/if}-->
-				<!--{if $_G['cache']['forumlinks'][1]}-->
-					<div class="mbn cl">
-						$_G['cache']['forumlinks'][1]
-					</div>
-				<!--{/if}-->
-				<!--{if $_G['cache']['forumlinks'][2]}-->
-					<ul class="x mbm cl">
-						$_G['cache']['forumlinks'][2]
-					</ul>
-				<!--{/if}-->
+				<!--{/loop}-->
 			</div>
 		</div>
 		<!--{/if}-->
