7876da7effcea0ab96f696276e2a832caf579383
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-09-26 03:14:02 -0500

    fix(blog): retain author avatars in thread lists

--- template/discuz_blog/forum/forumdisplay_list_box.php
+++ template/discuz_blog/forum/forumdisplay_list_box.php
@@ -110,6 +110,13 @@
 							<!--{/if}-->
 							<div class="kmfoot">
 								<span class="kmpl">{if $thread['allreplies']}$thread['allreplies']{else}$thread['replies']{/if}</span><span class="kmck"><!--{if $thread['isgroup'] != 1 || empty($groupnames[$thread['tid']]['views'])}-->$thread['views']<!--{else}-->{$groupnames[$thread['tid']]['views']}<!--{/if}--></span>
+								<!--{if $_GET['view'] != 'me'}-->
+									<!--{if $thread['authorid'] && $thread['author']}-->
+										<a href="home.php?mod=space&uid=$thread['authorid']" c="1" class="kmimg" target="_blank" title="{lang author}: $thread['author']"><!--{avatar($thread['authorid'],'middle')}--></a>
+									<!--{else}-->
+										<a href="javascript:;" class="kmimg"><!--{avatar(0,'middle')}--></a>
+									<!--{/if}-->
+								<!--{/if}-->
 								<!--{if $thread['allreplies'] || $thread['replies']}-->
 								<cite><!--{if $thread['lastposter']}-->
 									<a href="{if $thread['digest'] != -2}home.php?mod=space&username=$thread['lastposterenc']{else}forum.php?mod=viewthread&tid=$thread['tid']&page={echo max(1, $thread['pages'])}{/if}" target="_blank">$thread['lastposter']</a>
