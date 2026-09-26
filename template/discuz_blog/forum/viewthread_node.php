f725bc3fe97661b170cd72ed1beb7e31dfd15ebf
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-09-26 01:34:08 -0500

    Move views and the author filter into the blog left bar
    They belong to the left column, not the post meta row. X5 shows both there, in
    the stock left bar it inherits from the default theme, and the blog left column
    already had the author filter markup that was suppressed for the first post.
    
    Prints view and reply counts in the first post left bar block alongside the
    author name, and restores the filter for every post, matching the default
    markup x5 falls back to.

--- template/discuz_blog/forum/viewthread_node.php
+++ template/discuz_blog/forum/viewthread_node.php
@@ -25,6 +25,7 @@ $postshowavatars = !($_G['setting']['bannedmessages'] & 2 && ($post['memberstatu
 				<!--{if $_G['setting']['authoronleft']}-->
 					<div class="pi">
 						<div class="authi"><a href="home.php?mod=space&uid=$post['authorid']" target="_blank" class="xw1"{if $post[groupcolor]} style="color: $post[groupcolor]"{/if}>$post[author]</a>$authorverifys</div>
+						<!--{if $post['first']}--><div class="authi"><em>{lang show}: <!--{echo dnumber($_G[forum_thread][views])}--></em><span class="pipe">|</span><em>{lang reply}: <!--{echo dnumber($_G[forum_thread][allreplies])}--></em></div><!--{/if}-->
 					</div>
 				<!--{/if}-->
 				<div class="p_pop blk bui card_gender_$post['gender']" id="userinfo$post[pid]" style="display: none; {if $_G['setting']['authoronleft']}margin-top: -11px;{/if}">
@@ -84,12 +85,10 @@ $postshowavatars = !($_G['setting']['bannedmessages'] & 2 && ($post['memberstatu
 				<!--{hook/viewthread_sidetop $postcount}-->
 				<!--{if !(!empty($_G['setting']['threadguestlite']) && !$_G['uid'])}-->
 					{eval viewthread_profile_node('left', $post);}
-					<!--{if !($post['first'] && $_G['style']['blog_mod'])}-->
 					<!--{if !IS_ROBOT && !$_GET['authorid'] && !$_G['forum_thread']['archiveid']}-->
 						<p class="sg-author-filter"><a href="forum.php?mod=viewthread&tid=$post[tid]{if $page>1}&page=$page{/if}&authorid=$post[authorid]" rel="nofollow">{lang thread_show_author}</a></p>
 					<!--{elseif !$_G['forum_thread']['archiveid']}-->
 						<p class="sg-author-filter"><a href="forum.php?mod=viewthread&tid=$post[tid]{if $page>1}&page=$page{/if}" rel="nofollow">{lang thread_show_all}</a></p>
-					<!--{/if}-->
 					<!--{/if}-->
 					<!--{if $post[authorid] != $_G[uid]}-->
 						<ul class="xl xl2 o cl">
@@ -228,14 +227,6 @@ $postshowavatars = !($_G['setting']['bannedmessages'] & 2 && ($post['memberstatu
 					<em id="authorposton$post[pid]">{lang poston} $post[dateline]</em>
 				<!--{/if}-->
 				</span>
-				<!--{if $post['first'] && $_G['style']['blog_mod']}-->
-					<span class="pipe">|</span><em>$_G[forum_thread][views] {lang tmp007}</em>
-					<!--{if !IS_ROBOT && !$_GET['authorid'] && !$_G['forum_thread']['archiveid']}-->
-						<span class="pipe">|</span><a class="sg-author-filter" href="forum.php?mod=viewthread&tid=$post[tid]{if $page>1}&page=$page{/if}&authorid=$post[authorid]" rel="nofollow">{lang thread_show_author}</a>
-					<!--{elseif !$_G['forum_thread']['archiveid']}-->
-						<span class="pipe">|</span><a class="sg-author-filter" href="forum.php?mod=viewthread&tid=$post[tid]{if $page>1}&page=$page{/if}" rel="nofollow">{lang thread_show_all}</a>
-					<!--{/if}-->
-				<!--{/if}-->
 				<!--{if !IS_ROBOT && !$_G['forum_thread']['archiveid'] && $post['first'] }-->
 					<!--{if $_G['forum_thread']['attachment'] == 2 && $_G['group']['allowgetimage'] && (!$_G['setting']['guestviewthumb']['flag'] || $_G['setting']['guestviewthumb']['flag'] && $_G['uid'])}-->
 						<span class="pipe">|</span><a href="forum.php?mod=viewthread&tid=$_G[tid]&from=album">{lang view_bigpic}</a>
