6e5c87ad740bfea70422de0ea96d3b7ab500a1bb
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-09-26 02:24:24 -0500

    Remove blog's legacy notification popup menu
    header.php embedded the newer header_userstatus subtemplate, which defines its
    own ul#myprompt_menu for the notice dropdown, while header.php also emitted the
    classic ul#myprompt_menu.p_pop as a bare child of body. Two elements shared the
    id, and the two lookups disagreed: the subtemplate scopes its querySelector to
    .header-notice and gets the right element, but ajaxget resolves its target id
    globally and got the legacy one. Hovering the notice bell therefore overwrote
    the legacy menu's items with the markAsRead response and left it visible, which
    is the empty bar at the top of the page.
    
    The legacy menu was already unreachable: the theme has no #myprompt trigger
    link, and the ignore_notice cookie its dismiss item set is never read by PHP.
    The notice dropdown in the user status layer is the live path and is untouched.

--- template/discuz_blog/common/header.php
+++ template/discuz_blog/common/header.php
@@ -49,25 +49,6 @@
 		</div>
 
 		<!--{if !IS_ROBOT}-->
-			<!--{if $_G['uid']}-->
-			<ul id="myprompt_menu" class="p_pop" style="display: none;">
-				<!--{if !empty($_G['setting']['pmstatus'])}-->
-					<li><a href="home.php?mod=space&do=pm" id="pm_ntc" style="background-repeat: no-repeat; background-position: 0 50%;"><em class="prompt_news{if empty($_G[member][newpm])}_0{/if}"></em>{lang pm_center}</a></li>
-				<!--{/if}-->
-				<li><a href="home.php?mod=follow&do=follower"><em class="prompt_follower{if empty($_G[member][newprompt_num][follower])}_0{/if}"></em><!--{lang notice_interactive_follower}-->{if $_G[member][newprompt_num][follower]}($_G[member][newprompt_num][follower]){/if}</a></li>
-				<!--{if $_G[member][newprompt] && $_G[member][newprompt_num][follow]}-->
-					<li><a href="home.php?mod=follow"><em class="prompt_concern"></em><!--{lang notice_interactive_follow}-->($_G[member][newprompt_num][follow])</a></li>
-				<!--{/if}-->
-				<!--{if $_G[member][newprompt]}-->
-					<!--{loop $_G['member']['category_num'] $key $val}-->
-						<li><a href="home.php?mod=space&do=notice&view=$key"><em class="notice_$key"></em><!--{echo lang('template', 'notice_'.$key)}-->(<span class="rq">$val</span>)</a></li>
-					<!--{/loop}-->
-				<!--{/if}-->
-				<!--{if empty($_G['cookie']['ignore_notice'])}-->
-				<li class="ignore_noticeli"><a href="javascript:;" onClick="setcookie('ignore_notice', 1);hideMenu('myprompt_menu')" title="{lang temporarily_to_remind}"><em class="ignore_notice"></em></a></li>
-				<!--{/if}-->
-				</ul>
-			<!--{/if}-->
 			<!--{if $_G['uid'] && !empty($_G['style']['extstyle'])}-->
 				<div id="sslct_menu" class="cl p_pop" style="display: none;">
 					<!--{if !$_G[style][defaultextstyle]}--><span class="sslct_btn" onClick="extstyle('')" title="{lang default}"><i></i></span><!--{/if}-->
@@ -99,7 +80,7 @@
 					<!--{eval $mnid = getcurrentnav();}-->
 					<div class="dz_layer_nav">
 						<ul>
-							<!--{eval $mnid = getcurrentnav(); $n=0; $dz_menumore = ''; $nn = $_G['style']['top_navnum'] ? $_G['style']['top_navnum'] : 10;}-->
+							<!--{eval $mnid = getcurrentnav(); $n=0; $dz_menumore = ''; $visitedforumsmenu = ''; $nn = $_G['style']['top_navnum'] ? $_G['style']['top_navnum'] : 10;}-->
 							<!--{loop $_G['setting']['navs'] $nav}-->
 								<!--{if is_array($nav) && $nav['available'] && (!$nav['level'] || ($nav['level'] == 1 && $_G['uid']) || ($nav['level'] == 2 && $_G['adminid'] > 0) || ($nav['level'] == 3 && $_G['adminid'] == 1))}-->
 									<!--{eval $n++;}-->
@@ -113,6 +94,10 @@
 								<!--{/if}-->
 							<!--{/loop}-->
 							<li id="recentthreads_wrap" style="display:none"><a href="javascript:;" id="recentthreads" onmouseover="showMenu({'ctrlid':this.id,'pos':'43'})">{lang viewed_threads}</a></li>
+							<!--{if $_G['setting']['visitedforums']}-->
+								<!--{eval require_once libfile('function/forumlist'); empty($_G['cache']['forums']) && loadcache('forums'); $visitedforumsmenu = visitedforums();}-->
+								<!--{if $visitedforumsmenu}--><li><a href="javascript:;" id="header_visitedforums" onmouseover="showMenu({'ctrlid':this.id,'pos':'43'})">{lang viewed_forums}</a></li><!--{/if}-->
+							<!--{/if}-->
 						</ul>
 						<!--{hook/global_nav_extra}-->
 					</div>
@@ -136,6 +121,11 @@
 			<div id="recentthreads_menu" class="p_pop h_pop navs_menu" style="display:none">
 				<ul id="v_threads"></ul>
 			</div>
+			<!--{if $visitedforumsmenu}-->
+			<div id="header_visitedforums_menu" class="p_pop h_pop navs_menu" style="display:none">
+				<ul id="header_v_forums">$visitedforumsmenu</ul>
+			</div>
+			<!--{/if}-->
 			<script>
 			(function() {
 				var storageKey = 'kuing-recent-threads-v1';
